ad21db7fabcc6667335bf424de563feb04d974d8
richardjtelford <richard.telford@bio.uib.no>
2016-05-11 23:53:48 +0200

    convert many xls to one csv finished

--- xls_to_csv.R
+++ xls_to_csv.R
@@ -1,5 +1,6 @@
 #packages
 library("readxl")
+library("plyr")
 
 #list of xls files
 flist <- dir("./data", pattern = "*.xls", full.names = TRUE)
@@ -7,52 +8,83 @@ flist <- dir("./data", pattern = "*.xls", full.names = TRUE)
 #what do we have
 sapply(flist, excel_sheets)
 
-originDestination <- read.table(header = TRUE, sep = ",", text = "
-origin, destination
-HOTC, HOTC
-AOTC, AOTC
-MOTC, MOTC
-LOTC, LOTC
+originDestination <- read.table(header = TRUE, sep = ",", stringsAsFactors = FALSE, text = "
+  origin, destination
+  HOTC, HOTC
+  AOTC, AOTC
+  MOTC, MOTC
+  LOTC, LOTC
 
-HC, HC
-AC, AC
-MC, MC
-LC, LC
+  HC, HC
+  AC, AC
+  MC, MC
+  LC, LC
 
-H1, A1
-A1, M1
-M1, L1
+  HO, HO
+  AO, AO
+  MO, MO
+  LO, LO
 
-A2, H2
-M2, A2
-L2, M2
+  H1, A1
+  A1, M1
+  M1, L1
 
-H3, L3
+  A2, H2
+  M2, A2
+  L2, M2
 
-L4, H4
+  H3, L3
 
+  L4, H4
 ")  
                                 
                                 
-                    
-
-
 #import data, process and export to CSV
-lapply(flist, function(fl){
-  lapply(excel_sheets(fl), function(sheet){
+allsites <- lapply(flist, function(fl){
+  onesite <- lapply(excel_sheets(fl), function(sheet){
     dat <- read_excel(fl, sheet = sheet)
-    #testonly
-      dat <- read_excel(flist[1], sheet = excel_sheets(flist[1])[1])#test only
-    #testonly
+
+    #fix metadata
     dat <- dat[!is.na(dat$Measure), ]
     meta <- dat$DestinationSite
     meta[is.na(meta)] <- meta[which(is.na(meta))-1]#fill blanks in meta with row above
     dat$DestinationSite <- substr(meta, 1, 1)
     dat$DestinationBlock <- substr(meta, 1, 2)
-    dat$originPlotID <- mapvalues()
     dat$TTtreat <- substr(meta, 4, nchar(meta))
-    dat$destinationPlotID <- meta
-  })
+    dat$destinationPlotID <- meta    
+    dat$turfID <- meta
 
+    #find originplot
+    sitetreat <-  gsub("[[:digit:]]-", "",meta) #remove block info
+    originSitetreat <- mapvalues(sitetreat, from = originDestination$destination, to = originDestination$origin, warn_missing = FALSE )
+    dat$originPlotID <- paste0(substr(originSitetreat, 1, 1), substr(meta,2, 3), substr(originSitetreat, 2, nchar(originSitetreat)))
+
+    names(dat) <- trimws(names(dat))#zap trailing white space
+    
+    dat
+  })
+  do.call(rioja::Merge, args = onesite)
   
 })
+allsites <- do.call(rioja::Merge, args = allsites)
+
+originalNames <- names(allsites)
+#make save names
+names(allsites) <- make.names(names(allsites)) #make legal names
+
+#sort columns so all species together
+metaNames <- c("DestinationSite", "DestinationBlock", "originPlotID", "TTtreat", "destinationPlotID", "turfID", "RTtreat", "GRtreat",           "subPlot", "year", "date", "Measure", "recorder")
+
+envNames <- c("moss", "lichen", "litter", "soil", "rock", "totalVascular", "totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight", "litterThickness", "comment")
+
+sppNames <- names(allsites)[!names(allsites) %in% c(metaNames, envNames)]
+sppNames <- sort(sppNames)
+sppNames
+
+allsites <- allsites[,  c(metaNames, sppNames, envNames)]
+
+
+names(allsites)
+
+##write data to csv file
+write.csv(allsites, file = "allsites.csv", row.names = FALSE)
