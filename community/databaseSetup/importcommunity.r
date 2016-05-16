168042383ae0745c0d667042d9e98fea515c7da1
richardjtelford <richard.telford@bio.uib.no>
2016-05-16 16:52:50 +0200

    data now uploads

--- community/databaseSetup/importcommunity.r
+++ community/databaseSetup/importcommunity.r
@@ -4,40 +4,20 @@
 #merge subturf taxa
 ####################
 
-#for loop
-import.data<-function(filelist, con){
+import.data<-function(dat){#dat is data.frame from the correctly formatted csv file loaded into R
   require(dplyr)
   require(plyr)
-  sapply(filelist,function(n){     
-  
-                                                                              #uncomment to loop
-    #n<-"O:\\data\\SEEDCLIM2014\\Seedclim 2012 sp.fix CSV\\Lavisdalen2012 sp.fix.csv"                  #comment to loop
-    print(n)
-    chkft <- c("pleuro","acro", "liver", "lichen", "litter" ,"soil", "rock", "totalVascular", "totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight")
-    dat <- read.csv2(n, dec=",")  
-    if(ncol(dat) > 1){
-      if(any(sapply(dat[, chkft], class) == "factor")) 
-        dat <- read.csv2(n, dec=".")  
-    }else{
-      dat <- read.csv(n, dec=".")
-      if(any(sapply(dat[, chkft], class) == "factor"))
-        dat <- read.csv(n, dec=",")
-     } 
-         
+
     dat <- dat[!is.na(dat$originPlotID),]
     head(dat)
     names(dat)
-    dat$turfID <- trimws(dat$turfID)
- 
-   
+
   print(max(nchar(as.character(dat$comment)))) #how long is longest comment)
     
     #extract turf data
-    turf <- dat[,c("turfID", "TTtreat", "RTtreat", "GRtreat", "originPlotID", "destinationPlotID")]
+    turf <- dat[,c("turfID", "TTtreat", "originPlotID", "destinationPlotID")]
     turf <- unique(turf)
     turf$TTtreat <- trimws(turf$TTtreat) #  trim white spaces
-    turf$RTtreat <- trimws(turf$RTtreat)
-    turf$GRtreat <- trimws(turf$GRtreat)
   
     turf
     names(turf)
@@ -45,49 +25,48 @@ import.data<-function(filelist, con){
     alreadyIn <- dbGetQuery(con,"select turfId from turfs")$turfId
     newTurfs <- turf[!as.character(turf$turfID) %in% alreadyIn,] #find which turfs IDs are not already in database
     
-    if(nrow(newTurfs) > 0) sqlAppendTable(con, "turfs", newTurfs, row.names = FALSE)
+    if(nrow(newTurfs) > 0) dbWriteTable(con, "turfs", newTurfs, row.names = FALSE, append = TRUE)
     nrow(turf)
     nrow(newTurfs)
     
     message("done turfs")                                  
     
-    
     #subTurf env
-    subturfEnv <- dat[dat$Measure != "Cover", c("turfID", "subPlot", "year", "pleuro", "acro", "liver", "lichen", "litter", "soil", "rock", "comment")]
+    subturfEnv <- dat[dat$Measure != "Cover", c("turfID", "subPlot", "year", "moss", "lichen", "litter", "soil", "rock", "comment")]
     names(subturfEnv)[2] <- "subTurf"
-    if(!is.null(dat$missing)){
-       bad = dat$missing[dat$Measure != "Cover"]
-       bad[is.na(bad)] <- ""
-      subturfEnv <- cbind(subturfEnv, bad = bad)
-    } else{
-      subturfEnv <- cbind(subturfEnv, bad = "")    
-    }
+      if(!is.null(dat$missing)){
+         bad = dat$missing[dat$Measure != "Cover"]
+         bad[is.na(bad)] <- ""
+        subturfEnv <- cbind(subturfEnv, bad = bad)
+      } else{
+        subturfEnv <- cbind(subturfEnv, bad = "")    
+      }
     subturfEnv 
-    sqlAppendTable(con, "subTurfEnvironment", subturfEnv, row.names = FALSE)
+    dbWriteTable(con, "subTurfEnvironment", subturfEnv, row.names = FALSE, append = TRUE)
     nrow(subturfEnv)
     
     #TurfEnv
-    turfEnv <- dat[dat$Measure == "Cover", c("turfID","year",  "pleuro", "acro", "liver", "lichen", "litter", "soil", "rock", "totalVascular","totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight", "comment","recorder", "date")]
+    turfEnv <- dat[dat$Measure == "Cover", c("turfID","year",  "moss", "lichen", "litter", "soil", "rock", "totalVascular","totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight", "litterThickness", "comment", "recorder", "date")]
     if(any(nchar(as.character(turfEnv$comment[!is.na(turfEnv$comment)])) > 255)) {
       stop ("more than 255 characters in a comment field in turfEnv")
     }
-    sqlAppendTable(con, "turfEnvironment", turfEnv, row.names = FALSE)
+    dbWriteTable(con, "turfEnvironment", turfEnv, row.names = FALSE, append = TRUE)
   nrow(turfEnv)   
   
   #Mergedistionary
-  mergedictionary <- dbGetQuery(con,"SELECT * FROM mergedictionary")  
+#  mergedictionary <- dbGetQuery(con,"SELECT * FROM mergedictionary")  
   
     #TurfCommunity  
-  spp <- cbind(dat[, c("turfID", "year")], dat[, (which(names(dat) == "recorder") + 1) : (which(names (dat) == "pleuro")-1) ])[dat$Measure == "Cover",]
+  spp <- cbind(dat[, c("turfID", "year")], dat[, (which(names(dat) == "recorder") + 1) : (which(names (dat) == "moss")-1) ])[dat$Measure == "cover%",]
   spp[, 3 : ncol(spp)] <- colwise(as.numeric)(spp[, 3 : ncol(spp)])
-  notInMerged <- setdiff(names(spp)[-(1:2)], mergedictionary$oldID)
-  mergedictionary <- rbind(mergedictionary, cbind(oldID = notInMerged, newID = notInMerged))
-  mergedNames <- mapvalues(names(spp)[-(1:2)], from = mergedictionary$oldID, to = mergedictionary$newID, warn_missing = FALSE)
-  sppX <- lapply(unique(mergedNames), function(n){
-    rowSums(spp[, names(spp) == n, drop = FALSE])
-    })
-  sppX <- setNames(as.data.frame(sppX), unique(mergedNames))
-  spp <- cbind(spp[, 1:2], sppX)
+#  notInMerged <- setdiff(names(spp)[-(1:2)], mergedictionary$oldID)
+#  mergedictionary <- rbind(mergedictionary, cbind(oldID = notInMerged, newID = notInMerged))
+#  mergedNames <- mapvalues(names(spp)[-(1:2)], from = mergedictionary$oldID, to = mergedictionary$newID, warn_missing = FALSE)
+#  sppX <- lapply(unique(mergedNames), function(n){
+#    rowSums(spp[, names(spp) == n, drop = FALSE])
+#    })
+#  sppX <- setNames(as.data.frame(sppX), unique(mergedNames))
+ # spp <- cbind(spp[, 1:2], sppX)
     unique(as.vector(sapply(spp[, -(1:2)], as.character)))    #oddity search
     table(as.vector(sapply(spp[, -(1:2)], as.character)), useNA = "ifany") 
 
@@ -102,7 +81,7 @@ import.data<-function(filelist, con){
       spp2 <- spp2[spp2$cover > 0, ]
       spp2
     })
-    sqlAppendTable(con, "turfCommunity", sppT, row.names=FALSE)
+  dbWriteTable(con, "turfCommunity", sppT, row.names=FALSE, append = TRUE)
   
   
      #Check rows query for TurfCommunity :
@@ -122,29 +101,29 @@ import.data<-function(filelist, con){
                                               
      #subTurfCommunity  
      message("subturfcommunity")  
-    subspp <- cbind(dat[, c("turfID", "year", "subPlot")], dat[, (which(names(dat) == "recorder") + 1) : (which(names(dat) == "pleuro") -1) ])[dat$Measure != "Cover",]
+    subspp <- cbind(dat[, c("turfID", "year", "subPlot")], dat[, (which(names(dat) == "recorder") + 1) : (which(names(dat) == "moss") -1) ])[dat$Measure != "cover%",]
     subspp[subspp == 0] <- NA
-    subsppX <- lapply(unique(mergedNames), function(sppname){
-      species <- subspp[, names(subspp) == sppname, drop = FALSE]
-      if (ncol(species) == 1) {
-        return(species)
-      } else {
-        apply (species, 1, function(r) {
-          occurence <- which(!is.na(r))
-          if(length(occurence) == 0) return(NA)
-          if(length(occurence) == 1) return(r[occurence])
-          else {
-            warning(paste("more than one species observation in same subplot!"))
-            write.csv(data.frame(filename = n, species = species, occurence = r[occurence]), file = "cooccurence_log.csv", append = TRUE)
-            return(r[occurence][1])
-          }
-        })
-      }
-    })
+#    subsppX <- lapply(unique(mergedNames), function(sppname){
+#      species <- subspp[, names(subspp) == sppname, drop = FALSE]
+#      if (ncol(species) == 1) {
+#        return(species)
+#      } else {
+#        apply (species, 1, function(r) {
+#          occurence <- which(!is.na(r))
+#          if(length(occurence) == 0) return(NA)
+#          if(length(occurence) == 1) return(r[occurence])
+#          else {
+#            warning(paste("more than one species observation in same subplot!"))
+#            write.csv(data.frame(filename = n, species = species, occurence = r[occurence]), file = "cooccurence_log.csv", append = TRUE)
+#            return(r[occurence][1])
+#          }
+#        })
+#      }
+#    })
     
     
-    subsppX <- setNames(as.data.frame(subsppX), unique(mergedNames))
-    subspp <- cbind(subspp[, 1:3], subsppX)
+#    subsppX <- setNames(as.data.frame(subsppX), unique(mergedNames))
+#    subspp <- cbind(subspp[, 1:3], subsppX)
     unique(as.vector(sapply(subspp[, -(1:3)], as.character))) #oddity search
     print(table(as.vector(sapply(subspp[, -(1:3)], as.character)))) #oddity search
     
@@ -200,43 +179,7 @@ import.data<-function(filelist, con){
      sum(tolower(substring(tmp, 0,3)) == tolower(substring(as.character(dat$OriginSite[1]), 0,3)))#Check actual number of rows added for 2009 data:
   } ) 
   
-  
-  
-    
-    
-  ############### Vigdis seedling problem #only for 2011 data     #############################
-  if(dat$year[1] == 2011){
-    seed <- dat[dat$TTtreat != "" & dat$Measure != "Cover", c("turfID","subPlot", "year", "seedlings", "recorder")]  #get data.frame of seedlings      N1
-    seed$subPlot <- as.integer(as.character(seed$subPlot))
-    seed$turfID <- factor(seed$turfID)
-    seedsum <- dbGetQuery(con, paste("select * from [number identified seedlings by subplot] where siteID='",dat$DestinationSite[1], "' and Year=2011", sep=""))     #sqlQuery database for number of seedlings per subplot N2
-    seed <- seed[order(seed$turfID, seed$subPlot),]
-  
-    head(seed)
-    head(seedsum)
-    identical(seed[,1:2], seedsum[,1:2])#$turfID)
-    identical(seed[,2], seedsum[,2])
-  
-    identical(seed[,1], seedsum[,1])
-    
-    seed <- seed[!paste(seed$turf, seed$subPlot) %in% setdiff(paste(seed$turf, seed$subPlot), paste(seedsum$turf, seedsum$subTurf)),]#   then remove any missing rows as they have no species
-    
-    seed$seedlings[is.na(seed$seedlings)] <- 0
-  
-    seed$seedlings2 <- seed$seedlings
-    seed$seedlings2[seed$recorder == "W"]<-seed$seedlings[seed$recorder == "W"]-seedsum$SumOfseedlings[seed$recorder == "W"]#for VV /W subplots n seedlings N1 =  N1 - N2
-  
-    data.frame(seed$recorder, seed$seedlings, seedsum$SumOfseedlings, seed$seedlings2)
-  
-    #insert N1 into subTurfCommunity as unident seedling
-  
-    seed <- seed[seed$seedlings2 > 0,]
-    seed <- data.frame(turfID = seed$turfID, year = seed$year, subTurf = seed$subPlot, species = "seed.unid", seedlings = seed$seedlings2, juvenile = 0,adult = 0,fertile = 0,vegetative = 0,dominant = 0, cf = 1)
-    sqlAppendTable(con, "subTurfCommunity", seed, row.names=FALSE)
-  }
-  ######################### vigdis seedling problem fixed  #########################
-  
-  })                                                                                                      #uncomment to loop
+
 }
 
 
--- community/databaseSetup/inject_site_taxa.R
+++ community/databaseSetup/inject_site_taxa.R
@@ -11,6 +11,8 @@ dat <- read.csv ("community/databaseSetup/data/allsites.csv")
 con <- make_connection(username = "gbsrt", password = "b5b5b5", dbname = "transplant")
 
 
+
+
 #taxa - replace with full info
 spp <- names(dat)
 meta <-c("DestinationSite", "DestinationBlock", "originPlotID", "TTtreat", "destinationPlotID", "turfID", "RTtreat", "GRtreat", "subPlot", "year", "date", "Measure", "recorder", "moss", "lichen", "litter", "soil", "rock", "totalVascular", "totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight", "litterThickness", "comment"  )
@@ -38,5 +40,6 @@ dbWriteTable(con, "plots", value = plots, row.names = FALSE, append = TRUE)
 turfs <- setNames(data.frame(unique(dat[, c("turfID", "TTtreat", "originPlotID", "destinationPlotID")])), c("turfID", "TTtreat", "originPlotID", "destinationPlotID"))
 dbWriteTable(con, "turfs", value = turfs, row.names = FALSE, append = TRUE)
 
+import.data(dat)
 
 dbDisconnect(con)
