edcebc12e2478a265dfd64740e2e9a745263bb72
richardjtelford <richard.telford@bio.uib.no>
2016-05-18 19:35:26 +0200

    Aud species list made Transplant friendly

--- community/FieldForms/SpListPheno.R
+++ community/FieldForms/SpListPheno.R
@@ -1,31 +1,39 @@
 # Quiery for sp table: 6 sites, only 2009 and 2013 data, only herbs and grasses, excluding RTC
 
 
-db<-"O:\\Aud\\Projects work\\Postdoc -Bergen\\Seedclim data\\seedclim_2014-5-20 - Copy.mdb" #edit this line with correct location
-con<-odbcConnectAccess2007(db)
-
-##moss data
-phenQ<-"SELECT sites.siteID, blocks.blockID, blocks_1.blockID, turfs.TTtreat, turfs.turfID, new_TurfCommunity.Year, new_TurfCommunity.species, new_TurfCommunity.cover, sites.Temperature_level, sites.Precipitation_level, taxon.functionalGroup
-FROM taxon INNER JOIN (sites AS sites_1 INNER JOIN (sites INNER JOIN ((blocks AS blocks_1 INNER JOIN plots AS plots_1 ON blocks_1.blockID = plots_1.blockID) INNER JOIN (((blocks INNER JOIN plots ON blocks.blockID = plots.blockID) INNER JOIN turfs ON plots.plotID = turfs.originPlotID) INNER JOIN new_TurfCommunity ON turfs.turfID = new_TurfCommunity.turfID) ON plots_1.plotID = turfs.destinationPlotID) ON sites.siteID = blocks.siteID) ON sites_1.siteID = blocks_1.siteID) ON taxon.species = new_TurfCommunity.species
-WHERE ((Not (turfs.TTtreat)='') AND ((new_TurfCommunity.Year)=2009 Or (new_TurfCommunity.Year)=2013) AND ((sites.Temperature_level)<3) AND ((sites.Precipitation_level)>1) AND (Not (taxon.functionalGroup)='woody' And Not (taxon.functionalGroup)='pteridophyte'))
-ORDER BY taxon.functionalGroup;"
-phen<- sqlQuery(con,phenQ)
+con <- make_connection(username = "gbsrt", password = "b5b5b5", dbname = "transplant")
 
 
+##not moss data
+phenQ<-"SELECT sites_1.siteID, blocks.blockID as origblock, blocks_1.blockID as destblock, turfs.TTtreat, turfs.turfID, turfCommunity.Year, turfCommunity.species, turfCommunity.cover
+FROM taxon INNER JOIN (sites AS sites_1 INNER JOIN (sites INNER JOIN ((blocks AS blocks_1 INNER JOIN plots AS plots_1 ON blocks_1.blockID = plots_1.blockID) INNER JOIN (((blocks INNER JOIN plots ON blocks.blockID = plots.blockID) INNER JOIN turfs ON plots.plotID = turfs.originPlotID) INNER JOIN turfCommunity ON turfs.turfID = turfCommunity.turfID) ON plots_1.plotID = turfs.destinationPlotID) ON sites.siteID = blocks.siteID) ON sites_1.siteID = blocks_1.siteID) ON taxon.species = turfCommunity.species;"
+phen<- dbGetQuery(con,phenQ)
 
-phen <- subset(phen, phen$siteID.1=="Skjellingahaugen") # Hogsete, Rambera, Veskre, Gudmedalen, Lavisdalen, Skjellingahaugen
-remove <- c("NID.seedling", "NID.gram", "Eup.sp", "Rhi.min", "Hyp.mac")
-phen <- phen[!(phen$species %in% remove),]
-splist<-by(phen,phen$turfID, function(turf){
-  sp2009<-turf$species[turf$Year==2009]
-  sp2013<-turf$species[turf$Year==2013]
-  sp2013<-setdiff(sp2013,sp2009)
-  sp2009<-paste("*",sp2009,"*")
-  sp<-c(as.character(turf$turfID[1]),sp2009, sp2013,rep("",3))
+head(phen)
+audsfun <- function(site, year = 2015) {
+  phen <- subset(phen, phen$siteID == site)
   
-})
-splist<-unlist(sapply(splist,I))
-df<-data.frame(splist, A="", B="", C="", D="")
-kable(df, row.names=FALSE)
+  splist <- by(phen, phen$turfID, function(turf) {
+    sp2012 <- turf$species[turf$Year == 2012]
+    sp2015 <- turf$species[turf$Year == year]
+    sp2015 <- setdiff(sp2015, sp2012)
+    sp2012 <- paste("*", sp2012, "*")
+    sp <- c(as.character(turf$turfID[1]), sp2012, sp2015, rep("", 3))
+  })
+  splist <- unlist(sapply(splist, I))
+  df <- data.frame(
+    splist,
+    A = "",
+    B = "",
+    C = "",
+    D = ""
+  )
+  knitr::kable(df, row.names = FALSE)
+}
 
+h <- audsfun("H")
+a <- audsfun("A")
+m<-audsfun("M")
+l<-audsfun("L")
 
+save(h, a, m, l, file = "audsSpeciesLists.Rdata")
