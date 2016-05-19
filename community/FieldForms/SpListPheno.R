5cf88b84ed2796899df9218273ec1956215565fc
richardjtelford <richard.telford@bio.uib.no>
2016-05-19 18:42:11 +0200

    added cover by site

--- community/FieldForms/SpListPheno.R
+++ community/FieldForms/SpListPheno.R
@@ -5,17 +5,17 @@ con <- make_connection(username = "gbsrt", password = "b5b5b5", dbname = "transp
 
 
 ##not moss data
-phenQ<-"SELECT sites_1.siteID, blocks.blockID as origblock, blocks_1.blockID as destblock, turfs.TTtreat, turfs.turfID, turfCommunity.Year, turfCommunity.species, turfCommunity.cover
+phenQ <-"SELECT sites_1.siteID, blocks.blockID as origblock, blocks_1.blockID as destblock, turfs.TTtreat, turfs.turfID, turfCommunity.year, turfCommunity.species, turfCommunity.cover
 FROM taxon INNER JOIN (sites AS sites_1 INNER JOIN (sites INNER JOIN ((blocks AS blocks_1 INNER JOIN plots AS plots_1 ON blocks_1.blockID = plots_1.blockID) INNER JOIN (((blocks INNER JOIN plots ON blocks.blockID = plots.blockID) INNER JOIN turfs ON plots.plotID = turfs.originPlotID) INNER JOIN turfCommunity ON turfs.turfID = turfCommunity.turfID) ON plots_1.plotID = turfs.destinationPlotID) ON sites.siteID = blocks.siteID) ON sites_1.siteID = blocks_1.siteID) ON taxon.species = turfCommunity.species;"
-phen<- dbGetQuery(con,phenQ)
+phen <- dbGetQuery(con, phenQ)
 
 head(phen)
 audsfun <- function(site, year = 2015) {
   phen <- subset(phen, phen$siteID == site)
   
   splist <- by(phen, phen$turfID, function(turf) {
-    sp2012 <- turf$species[turf$Year == 2012]
-    sp2015 <- turf$species[turf$Year == year]
+    sp2012 <- turf$species[turf$year == 2012]
+    sp2015 <- turf$species[turf$year == year]
     sp2015 <- setdiff(sp2015, sp2012)
     sp2012 <- paste("*", sp2012, "*")
     sp <- c(as.character(turf$turfID[1]), sp2012, sp2015, rep("", 3))
@@ -28,7 +28,7 @@ audsfun <- function(site, year = 2015) {
     C = "",
     D = ""
   )
-  knitr::kable(df, row.names = FALSE)
+  df
 }
 
 h <- audsfun("H")
@@ -37,3 +37,17 @@ m<-audsfun("M")
 l<-audsfun("L")
 
 save(h, a, m, l, file = "audsSpeciesLists.Rdata")
+
+
+#########control cover by site
+
+phenCont <- subset(phen, TTtreat %in% c("C", "O"))
+phenCont$pa <-phenCont$cover > 0
+
+sites <- list("H", "A", "M", "L")
+audslist <- llply(sites, function(site){print(site)
+  xtabs(pa ~ species + year, data = subset(phenCont, siteID == site))
+})
+names(audslist) <- sites
+
+save(audslist, file = "audsInControl.Rdata")
