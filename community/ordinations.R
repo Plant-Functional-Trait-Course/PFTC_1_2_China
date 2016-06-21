a21b7ac982df0ecb4c5b5a3e54e9b24912c432b4
richardjtelford <richard.telford@bio.uib.no>
2016-06-21 19:39:53 +0200

    Beginning Hogsete plots, large start year

--- community/ordinations.R
+++ community/ordinations.R
@@ -24,11 +24,23 @@ autoplot(CA)
 fCA <- fortify(CA, display = "sites")
 fCA <- cbind(fCA, cover_meta[cover_meta$TTtreat %in% c("control", "local"), ])
 
-ggplot(fCA, aes(x = Dim1, y = Dim2, col = originSiteID, shape = TTtreat, group = originPlotID, linetype = TTtreat)) +
-  geom_point() +
+g <-ggplot(fCA, aes(x = Dim1, y = Dim2, col = originSiteID, shape = TTtreat, group = originPlotID, linetype = TTtreat)) +
+  geom_point(aes(size = ifelse(year == min(year), 2, 1))) +
   geom_line() + 
   coord_fixed(ratio = 1) +
+  scale_size(range = c(1, 2), guide = "none")
   labs(x = "CA1", y = "CA2", colour = "Site", shape = "Treatment", linetype = "Treatment")
-
-
+g
   
+#Hogsete plot. Site M
+M_cover <- cover[cover_meta$destSiteID == "M", ]
+M_cover <- M_cover[, colSums(M_cover >0) >1]
+decorana(sqrt(M_cover))
+
+# CA
+MCA <- cca(sqrt(M_cover))
+
+fMCA <- fortify(MCA, display = "sites")
+fMCA <- cbind(fMCA, cover_meta[cover_meta$destSiteID == "M", ])
+
+g %+% fMCA
