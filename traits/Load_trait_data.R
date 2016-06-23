4622368981175098daa40f8f6c3d69bcb24e8e48
audhalbritter <me@audhalbritter.com>
2016-06-23 13:59:20 +0200

    Change Bistorta to Polygonum

--- traits/Load_trait_data.R
+++ traits/Load_trait_data.R
@@ -13,9 +13,19 @@ str(trait.site)
 trait.site$Date <- ymd(trait.site$Date) # convert date
 trait.site$Leaf_Thickness_Ave_mm <- rowMeans(trait.site[,c("Leaf_Thickness_1_mm", "Leaf_Thickness_2_mm", "Leaf_Thickness_3_mm","Leaf_Thickness_4_mm","Leaf_Thickness_5_mm","Leaf_Thickness_6_mm")], na.rm = TRUE) # calculate average leaf thickness
 
+# Change Bistorta to Polygonum
+trait.site$Taxon_TNRS_corrected[trait.site$Taxon_TNRS_corrected=="Bistorta vivipara"] <- "Polygonum viviparum"
 
+# Replace missing site values from elevation
+index <- as.vector(unique(trait.site$Elevation))
+values <- c("L", "M", "A", "H")
+Site.Check <- values[match(trait.site$Elevation, index)]
+trait.site$Site == Site.Check
 
+trait.site[trait.site$Site == "", "Site"] <- values[match(trait.site$Elevation, index)]
+ 
 
+# Get an idea how many species are measured in common with trait data set
 taxa <- dbGetQuery(con, "SELECT * FROM taxon")
 
 sp.comparison <- plyr::ldply(unique(trait.site$Taxon_TNRS_corrected), function(x){
@@ -25,3 +35,24 @@ sp.comparison <- plyr::ldply(unique(trait.site$Taxon_TNRS_corrected), function(x
 })
 
 ggplot(data.frame(noccur = colSums(cover > 0), inTraits = names(cover) %in% sp.comparison$commCode), aes(x = inTraits, y = noccur)) + geom_boxplot()
+
+
+# calculate nr traits measured
+# Number of traits measured per site and species
+traits <- trait.site[,c(8:9,11,19:21)]
+
+by(trait.site, list(trait.site$Site, trait.site$Taxon_TNRS_corrected), function(x){
+  ...
+})
+
+# Community species list not in trait data:
+...
+
+# How many species have no traits? Which species have more than 3 or 5 trait values.
+# Number of indivivduals and leaves per site and species
+by(trait.site, trait.site$Site, function(x){
+  table(x$Taxon_TNRS_corrected, x$Leaf_Number)
+  
+})
+
+
