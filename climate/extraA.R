c343d14a68ba869a0d3fca4a620885397922de79
richardjtelford <richard.telford@bio.uib.no>
2016-12-06 15:44:46 +0100

    initial commit

---
+++ climate/extraA.R
@@ -0,0 +1,25 @@
+#read extra site A data (another site in the vicinity of site A)
+
+#import data
+f <- "climate/data/extraSiteA/climate data_3800m_YJG.xls"
+
+col_names <-c("dateTime", "waterContent20", "Tsoil20", "waterContent10", "Tsoil10", "RH", "Tair25", "PAR")
+
+extraA <- gdata::read.xls(f, sheet = 1, header = FALSE, skip = 4, stringsAsFactors = FALSE, na.strings = "#N/A!", comment = "")
+names(extraA) <- col_names
+extraA$dateTime <- as.POSIXct(extraA$dateTime, format = "%Y-%m-%d %I:%M %p", tz = "Asia/Shanghai")
+
+#remove NA columns
+
+extraA <- extraA[, !is.na(names(extraA))] 
+
+##some plots - look OK (except for initial gap in +25 measurements)
+
+ggplot(extraA, aes(x = dateTime, y = Tair25)) + geom_path() 
+ggplot(extraA, aes(x = dateTime, y = RH)) + geom_path() 
+ggplot(extraA, aes(x = dateTime, y = Tsoil20)) + geom_path() 
+ggplot(extraA, aes(x = dateTime, y = Tsoil10)) + geom_path() 
+
+ggplot(extraA, aes(x = dateTime, y = waterContent20)) + geom_path() 
+ggplot(extraA, aes(x = dateTime, y = waterContent10)) + geom_path() 
+
