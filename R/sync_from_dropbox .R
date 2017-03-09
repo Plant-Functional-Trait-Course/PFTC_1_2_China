3155712ee6e2251a2f1bd9f8983a5e9812f63edd
richardjtelford <richard.telford@bio.uib.no>
2017-03-09 06:24:07 +0100

    clean, comment, todo list

--- R/sync_from_dropbox .R
+++ R/sync_from_dropbox .R
@@ -1,41 +1,38 @@
+## ToDo
+#make delta test specific to one file
+#test
+# check behaviour if directory is missing
+# remember user between sessions?
 
-sync_from_dropbox <- function(drop_path, drop_file, local_path){
+sync_from_dropbox <- function(drop_path, drop_file, local_path_file){
   
   if(!require("rdrop2")){
-    message("Install rdrop2 with devtools::install_github('karthik/rdrop2') or from CRAN")
-    return()
+    stop("Need to install rdrop2 with devtools::install_github('karthik/rdrop2') or from CRAN")
   }
   browser()
   drop_acc()# get account - will ask for login details
   #drop_auth()
   
-  
-  
-  
-  #download
-  local_path <- "community/data/transplant.sqlite"
-  drop_path <-"/transplant/community/data"
-  
-  if(file.exists(local_path)){
-    load(paste0(local_path, ".Rdata"))#load previously stored deltas
+  if(file.exists(local_path_file)){
+    load(paste0(local_path_file, ".Rdata"))#load previously stored deltas
     d_delta <- drop_delta(cursor = dropbox_delta$cursor, path_prefix = drop_path)#check if files changed
-    if(nrow(d_delta$entries) > 0){#zero rows if no change
+    if(nrow(d_delta$entries) > 0){#zero rows if no changes
       downloadneeded <- TRUE
-    } else{
+    } else{#local copy up to date
       downloadneeded <- FALSE
     }
-  } else{
+  } else{#no local copy
     downloadneeded <- TRUE
   }
   
   
-  if(needdownload){
-    drop_get(path = paste0(drop_path, drop_file) , local_file = local_path, overwrite = TRUE)
+  if(downloadneeded){
+    drop_get(path = paste0(drop_path, drop_file) , local_file = local_path_file, overwrite = TRUE)
     
     #get deltas so can check for updates later
     dropbox_delta <- drop_delta(path_prefix = drop_path)
-    save(dropbox_delta, file = paste0(local_path, ".Rdata"))
+    save(dropbox_delta, file = paste0(local_path_file, ".Rdata"))
   }
 }
 
-sync_from_dropbox(drop_path = "test/", drop_file = "test.txt", local_path = "community/test.txt")
+sync_from_dropbox(drop_path = "test/", drop_file = "test.txt", local_path_file = "community/test.txt")
