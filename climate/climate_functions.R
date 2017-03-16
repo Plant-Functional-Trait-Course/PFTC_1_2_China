8296f8903b36663a043bc292382b739db1375df1
audhalbritter <me@audhalbritter.com>
2017-03-16 13:59:33 +0100

    almost fix ZoomIntoPlot function

--- climate/climate_functions.R
+++ climate/climate_functions.R
@@ -46,15 +46,15 @@ CalcYearlyData<-function(dd){
 #### ZOOM INN PLOT FUNCTION ####
 ZoomIntoPlot <- function(dd, use.gather, date1, date2, site, variable){
   if(use.gather == "yes"){
-    dd <- dd %>% 
+    dd %>% 
       gather(key = variable, value = value, -dateTime, -site) %>% 
-      filter(dateTime > date1, dateTime < date2, site == "site", variable == "variable")
-    ggplot(dd, aes(x = dateTime, y = value)) + geom_line()
+      filter(dateTime > date1, dateTime < date2, site == site, variable == variable) %>% 
+      ggplot(aes(x = dateTime, y = value)) + geom_line()
   }
   else if(use.gather == "no"){
-    dd <- dd %>% 
-      filter(dateTime > date1, dateTime < date2, site == "site")
-    ggplot(dd, aes(x = dateTime, y = variable)) + geom_line()
+    dd %>% 
+      filter(dateTime > date1, dateTime < date2, site == site) %>% 
+      ggplot(aes(x = dateTime, y = variable)) + geom_line()
   }
 }
 
