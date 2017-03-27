a1b68c5c726e326e7dda6c91ccbb38f66e5dac8a
audhalbritter <me@audhalbritter.com>
2017-03-27 20:34:38 +0200

    correct waterContent5

--- climate/R/read.climate2.r
+++ climate/R/read.climate2.r
@@ -6,9 +6,9 @@ library("tidyverse")
 source("climate/R/read_badly_formatted_data.R") # badly formatted data in data/csv
 source("climate/R/read_well_formatted_data.R") # better formatted data in data/2016
 weather2 <- weather #make safe copy
-
+datsaftey <- dat
 #combine output of well and badly formatted dat
-weather <- bind_rows(weather2, dat)
+weather <- bind_rows(weather2, datsaftey)
 
 #convert site to factor (to ensure plotting order)
 weather$site <- factor(weather$site, levels = c("H", "A", "M", "L"))
@@ -36,10 +36,10 @@ weather <- weather %>%
   mutate(Tsoil0 = ifelse(Tsoil0 > 60 | Tsoil0 < -50, NA, Tsoil0)) %>%
   mutate(Tsoil5 = ifelse(Tsoil5 > 50 | Tsoil5 < -5, NA, Tsoil5)) %>%
   mutate(Tsoil20 = ifelse(Tsoil20 > 50 | Tsoil20 < -5, NA, Tsoil20)) %>% 
-  mutate(windSpeed = ifelse(windSpeed > 50, NA, windSpeed))
+  mutate(windSpeed = ifelse(windSpeed > 50, NA, windSpeed)) %>% 
+  mutate(windDirection = ifelse(windDirection < 0, NA, windDirection))
 
 #remove minimal variance in Tsoil0 Toil5
-
 weather <- weather %>% 
   group_by(file) %>%
   mutate(sd = sd(Tsoil5, na.rm = TRUE)) %>% #Tsoil5
@@ -77,20 +77,47 @@ weather <- weather %>%
   mutate(Tsoil0 = ifelse(site == "H" & season == "Spring" & Tsoil0 < -15, NA, Tsoil0)) %>% 
   # Tsoil20: remove spikes in winter 2015
   mutate(Tsoil20 = ifelse(season == "Winter" & site == "H" & Tsoil20 > 2.5 , NA, Tsoil20)) %>% 
+  # waterContenet5
+  mutate(waterContent5 = ifelse(nummonth %in% c(1,2,3,11,12), NA, waterContent5)) %>% # remove waterContent5 in winter: 1.11 - 30.3
+  mutate(waterContent5 = ifelse(site == "A" & waterContent5 > 0.5, NA, waterContent5)) %>%  # remove spikes
+  mutate(waterContent5 = ifelse(site == "H" & waterContent5 < 0.3, NA, waterContent5)) %>%  # remove spikes
+  mutate(waterContent5 = ifelse(site == "H" & year(dateTime) > 2015 & year(dateTime) < 2017, NA, waterContent5)) %>%  # remove spikes
   select(-nummonth, -season)
 
-save(weather, file = "climate/weather.Rdata")
 
+## delete bad data
+weather <- weather %>% 
+  # delete bad UV from H
+  mutate(UV = ifelse(site == "H", NA, UV)) %>% 
+  # delete bad waterContent5 from H
+  mutate(waterContent5 = ifelse(site == "H" & dateTime > as.POSIXct("2016-01-01 00:00:00", tz = "Asia/Shanghai") & dateTime < as.POSIXct("2016-09-24 01:00:00", tz = "Asia/Shanghai"), NA, waterContent5))
 
 
 ## deal with duplicates (from overlapping files)
+distinct_weather <- weather %>%
+  select(-extra, -notes, -Batt, -probableTempUnit) %>% 
+  gather(key = variable, value = value, -dateTime, -site, -file) %>% 
+  group_by(site, dateTime) %>% 
+  filter(!is.na(value))
+
+save(distinct_weather, file = "climate/clean_weather.Rdata")
+
+
+weather %>% ggplot(aes(x = dateTime, y = waterContent5)) + geom_line() + facet_wrap(~site)
+
+
+# some stuff with duplicates
+weather %>% 
+  group_by(file) %>% 
+  summarise_all(.funs = function(x) round(mean(is.na(x)), 1)) %>% 
+  print(n = 59)
 
 #check variance is low amongst duplicates
 weather %>% 
   group_by(dateTime, site) %>% 
-   ungroup() %>%
-    mutate(dateTime = ymd(format(dateTime, "%Y-%m-%d"))) %>%
-   select(-notes, -Batt, -probableTempUnit, -extra) %>%
+  ungroup() %>%
+  mutate(dateTime = ymd(format(dateTime, "%Y-%m-%d"))) %>%
+  select(-notes, -Batt, -probableTempUnit, -extra) %>%
   select(file, dateTime, site, Tsoil5, Tsoil0, Tsoil20, waterContent5, waterContent20) %>%
   group_by(file, dateTime, site) %>%
   mutate(n = n()) %>% 
@@ -103,8 +130,15 @@ weather %>%
   summarise(delta = diff(range(value))) %>% 
   arrange(desc(delta))
 
+# first approach to remove duplicates - start with last file
+# does not work
+#distinct_weather <- weather %>%   ungroup() %>% mutate(file  = reorder(file, dateTime, max, na.rm = TRUE)) %>%
+  #arrange(file) %>% distinct(site, dateTime, .keep_all = TRUE) %>%  select(-file, -probableTempUnit, -notes, -Batt, -extra)
 
 
+
+
+# some plots, no need to run!
 weather %>% filter(dateTime == ymd_hms("2014-04-18 13:30:00", tz = "Asia/Shanghai"), site == "H") %>% select(dateTime, site, PAR, Tair)
 
 
@@ -122,13 +156,13 @@ weather %>% filter(site == "H") %>% group_by(file) %>% summarise(cor = cor(rain,
 
 weather %>%
   ggplot(aes(x = windSpeed, y = rain, colour = site)) + 
-    geom_point() +
-    facet_wrap(~site, scales = "free_y") + 
-    xlim(c(NA, 26)) 
+  geom_point() +
+  facet_wrap(~site, scales = "free_y") + 
+  xlim(c(NA, 26)) 
 
 weather %>%
   ggplot(aes(x =rain, fill = site)) + 
-    geom_histogram() +
+  geom_histogram() +
   facet_wrap(~site, scales = "free")
 
 weather %>% filter(site == "A") %>% ggplot(aes(x = dateTime, y = windDirection, colour = site, group = file)) + geom_path() + facet_wrap(~site)
@@ -139,28 +173,3 @@ weather %>% filter(site == "A") %>% ggplot(aes(x = windSpeed, y = windDirection,
 
 #UV
 ggplot(weather, aes(x = dateTime, y = UV, colour = site, group  = file)) + geom_path() + facet_wrap(~site)
-
-## delete bad data
-weather <- weather %>% 
-  # delete bad UV from H
-  mutate(UV = ifelse(site == "H", NA, UV))
-
-weather <- weather %>% 
-  # delete bad waterContent5 from H
-  mutate(waterContent5 = ifelse(site == "H" & dateTime > as.POSIXct("2016-01-01 00:00:00", tz = "Asia/Shanghai") & dateTime < as.POSIXct("2016-09-24 01:00:00", tz = "Asia/Shanghai"), NA, waterContent5))
-
-
-
-
-
-
-#remove duplicates - start with last file 
-distinct_weather <- weather %>% 
-  ungroup() %>% 
-  mutate(file  = reorder(file, dateTime, max, na.rm = TRUE)) %>%
-  arrange(file) %>%
-  distinct(site, dateTime, .keep_all = TRUE) %>%
-  select(-file, -probableTempUnit, -notes, -Batt, -extra)
-
-save(distinct_weather, file = "climate/clean_weather.Rdata")
-
