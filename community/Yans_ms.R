2c1860014e86ca3711f8deb34fde8bc7600f68b7
richardjtelford <richard.telford@bio.uib.no>
2017-04-06 16:50:15 +0200

    climate calculations, richness plots

--- community/Yans_ms.R
+++ community/Yans_ms.R
@@ -6,10 +6,33 @@
 ##load packages
 library("vegan")
 library("ggvegan")
+library("lubridate")
 
 ##load data
 #climate
-load("climate/climate.Rdata")
+load("climate/climate_month.Rdata")
+summer_temp <- climate_month %>% 
+  filter(variable == "Tair") %>% 
+  mutate(month = month(month)) %>%
+  filter(month %in% 6:8) %>% 
+  group_by(logger, site) %>% 
+  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  arrange(site) %>%
+  ungroup()
+
+#control = Tair
+#OTC = OTC - Tair
+#Warm1 = TairD - TairO  
+
+pred_temp <- bind_rows(
+  summer_temp %>% filter(logger == "gradient") %>% mutate(TTtreat = "control"),#control
+  summer_temp %>% filter(logger == "gradient") %>% mutate(TTtreat = "local"),#local transplant (== control)
+  summer_temp %>% spread(key = logger, value = mean) %>% mutate(mean = otc - gradient, TTtreat = "OTC") %>% select(-gradient, -otc),
+  summer_temp %>% filter(logger == "gradient") %>% mutate(mean = lead(mean) - mean, TTtreat = "warm1") %>% filter(site != "L")
+)  %>% select(-logger) %>%
+  mutate(mean = if_else(site == "L" & TTtreat == "OTC", 2, mean))
+warning("using estiated OTC effect for site L")
+
 
 #community
 source("community/start_here.R")
@@ -33,7 +56,7 @@ cover_fat <- cover_thin %>%
 
 cover_fat_spp <- cover_fat %>% select(-(originSiteID:year))
 
-NMDS <- metaMDS(cover_fat_spp)#DNC
+NMDS <- metaMDS(cover_fat_spp, noshare = TRUE, try = 30)#DNC
 
 fNMDS <- fortify(NMDS) %>% 
   filter(Score == "sites") %>%
@@ -58,18 +81,22 @@ g
 ## responses
 #richness
 richnessC <- cover_thin %>% 
-  group_by(turfID, originBlockID, TTtreat, originSiteID, year) %>% 
-  summarise(n = n()) %>% 
+  group_by(turfID, originBlockID, TTtreat, originSiteID, year) %>%   summarise(n = n()) %>% 
   group_by(originBlockID, TTtreat, originSiteID, year) %>%
-  select(-turfID) %>%
-  spread(key = TTtreat, value = n)
+  select(-turfID) %>% 
+  full_join(pred_temp, by = c("originSiteID" = "site", "TTtreat" = "TTtreat")) %>%
+  mutate(contrast = if_else(TTtreat %in% c("local", "control"), 0, mean))
 
+gc <- ggplot(richnessC %>% filter(TTtreat %in% c("local", "control"), year == 2016), aes(x = mean, y = n, colour = originSiteID, shape = TTtreat, group = 1)) + 
+  geom_jitter(height = 0, width = 0.1) +
+  geom_smooth(method = "lm") +
+  ggtitle("Gradient")
 
-ggplot(richnessC, aes(x = year, y = control, colour = originSiteID)) + 
-  geom_jitter(height = 0, width = 0.2) + 
-  geom_smooth(aes(group = originSiteID))
+gw <- gc %+% (richnessC %>% filter(TTtreat %in% c("local", "warm1"), year == 2016)) + aes(x = contrast, group = originSiteID) +
+  ggtitle("Transplant")
 
-
-
-
- 
+go <- gw %+% (richnessC %>% filter(TTtreat %in% c("control", "OTC"), year == 2016)) + 
+  ggtitle("OTC")
+                                  
+gridExtra::grid.arrange(gc, gw, go)                                  
+                                  
