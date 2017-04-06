853a7fb4c85716dd53df64119c526982ef4e6485
audhalbritter <me@audhalbritter.com>
2017-04-06 09:35:14 +0200

    group data by site remove species from experiments

--- traits/traits_cover.R
+++ traits/traits_cover.R
@@ -10,14 +10,20 @@ trait2016 %>% mutate(loc1 = substr(Location, 1, 1)) %>%
 traits %>% 
   filter(Project %in% c("LOCAL", "0", "C")) %>%
   mutate(year = year(Date)) %>% 
-  group_by(year, Taxon) %>% 
+  group_by(year, Taxon, Site) %>% 
   summarise(n_leaves = n()) %>%
   spread(key = year, value = n_leaves, fill = "") %>%
   full_join(
-    cover_thin %>% group_by(speciesName) %>% summarise(nturfs = n(), max = max(cover)), 
-    by = c("Taxon" = "speciesName")
+    cover_thin %>% 
+      filter(TTtreat %in% c("control", "local")) %>% # can filter by year here
+      group_by(speciesName, originSiteID) %>% 
+      summarise(nturfs = n(), max = max(cover)), 
+    by = c("Taxon" = "speciesName", "Site" = "originSiteID")
       ) %>%
   arrange(Taxon) %>% 
+  mutate_all(function(x) if_else(is.na(x), "", as.character(x))) %>% 
+  # remove sp from Experiments
+  filter(!Taxon %in% c("Potentilla leuconota", "Plantago asiatica", "Polygonum viviparum", "Veronica szechuanica", "Viola biflora var. rockiana", "Pedicularis davidii", "Hypericum wightianum", "Geranium pylzowianum", "Epilobium fangii", "Artemisia flaccida")) %>% 
 #  slice(200:250) %>%
   print(n = 1000)
 
