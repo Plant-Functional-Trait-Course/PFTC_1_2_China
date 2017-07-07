572886ac4446ee896bc463c49fb7e2e3978a0776
audhalbritter <me@audhalbritter.com>
2017-07-07 08:51:08 +0200

    merge with trait 2015 data

--- traits/LeafArea2_2015.R
+++ traits/LeafArea2_2015.R
@@ -22,3 +22,20 @@ Newleafarea2015 <- leafarea2015 %>%
   mutate(File_Name = if_else(grepl("\\d-\\d$", File_Name), File_Name, gsub("(.*)(-\\d$)", "\\1-1\\2",  File_Name))) %>% # add missing Individual_numbers
   separate(col = File_Name, into = c("Date", "Site", "Elevation", "Species", "Individual_number", "Leaf_number"), sep = "-") %>% 
   mutate(Elevation = as.numeric(Elevation))
+
+
+
+# merge with trait 2015 data
+trait2015 %>% 
+  #### MORE THAN 1000 DID NOT JOIN !!! ####
+left_join(Newleafarea2015, by = c("Site", "Elevation", "Taxon", "Individual_number", "Leaf_number")) %>% 
+  
+  
+  
+  
+
+ggplot(aes(x = Dry_Mass_g, y = LeafArea2, colour = Site)) + 
+  geom_point() +   
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
+  scale_x_log10() + 
+  scale_y_log10()
