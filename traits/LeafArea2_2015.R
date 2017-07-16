1d1291c8fab3ff92b1d775ff82d1ed25045a2c95
audhalbritter <me@audhalbritter.com>
2017-07-16 13:24:59 +0200

    Fix new leaf area 2015 data to join to trait data

--- traits/LeafArea2_2015.R
+++ traits/LeafArea2_2015.R
@@ -1,18 +1,18 @@
 #### IMPORT LEAF AREA 2 CALCULATION FROM 2015 LEAVES ####
-
+library(tidyverse)
 library(lubridate)
 library(readr)
 
-leafarea2015 <- read_csv(file = "~/Desktop/Leaf.Area2015.csv")
+leafarea2015 <- read_csv(file = "traits/data/Leaf.Area2015-20170714.csv")
 
 Newleafarea2015 <- leafarea2015 %>% 
   select(-X1) %>% 
   rename(Taxon = X5) %>% 
-  filter(!is.na(LeafArea)) %>% # remove strange files
-  filter(!grepl("black|dirt|empty|double", comment)) %>% # remove black lines, dirt and douple scans
+  filter(!is.na(LeafArea)) %>% # remove files without area
+  filter(!grepl("black|dirt|empty|double|brown|yellow|folded", comment)) %>% # remove black lines, dirt and douple scans
   # split File name and Area 1, 2, 3
   separate(col = File_Name, into = c("File_Name", "Area"), sep = "\\.txt\\.") %>% 
-  # summarize areas 
+  # summarize areas from different part of  leaves
   group_by(File_Name, Taxon) %>%
   summarise(LeafArea2 = sum(LeafArea), allComments = paste(comment, collapse = " _ ")) %>% 
   ungroup() %>% 
@@ -20,22 +20,24 @@ Newleafarea2015 <- leafarea2015 %>%
   mutate(File_Name = gsub("3850_Potentilla-stenophylla", "3850-Potentilla_stenophylla", File_Name)) %>%
   mutate(File_Name = gsub("jpg", "", File_Name)) %>% # zap jpg without dot
   mutate(File_Name = if_else(grepl("\\d-\\d$", File_Name), File_Name, gsub("(.*)(-\\d$)", "\\1-1\\2",  File_Name))) %>% # add missing Individual_numbers
-  separate(col = File_Name, into = c("Date", "Site", "Elevation", "Species", "Individual_number", "Leaf_number"), sep = "-") %>% 
-  mutate(Elevation = as.numeric(Elevation))
+  mutate(File_Name = gsub("20150820-m-3500-Gentiana_crassuloides-S-1-1", "20150820-m-3500-Gentiana_crassuloides-1-1", File_Name)) %>%  # remove -S in 20150820-m-3500-Gentiana_crassuloides-S-1-1 Gentiana crassuloides
+  separate(col = File_Name, into = c("Date", "Site", "Elevation", "Species", "Individual_Number", "Leaf_Number"), sep = "-") %>% 
+  mutate(
+    Site = gsub("m", "M", Site), # replace m with M
+    Elevation = gsub("300", "3000", Elevation), # replace 300 with 3000, all are Site L
+    Elevation = gsub("3580", "3500", Elevation), # replace 3580 with 3500, only occurs at site M
+    # fix species names
+    Taxon = gsub("Neottianthe cucullata var.camcola", "Neottianthe cucullata Var.Camcola", Taxon),
+    Taxon = gsub("Carex nubigena", "Carex nubige", Taxon),
+    Taxon = gsub("\xa0", " ", Taxon),
+    Elevation = as.numeric(Elevation)
+    ) %>% 
+  select(-Species, -Date) # remove columns before merging
+
+# Leaf area caclculation did not work: Swertia macrosperma, Prenanthes macrophylla
 
 
-
-# merge with trait 2015 data
-trait2015 %>% 
-  #### MORE THAN 1000 DID NOT JOIN !!! ####
-left_join(Newleafarea2015, by = c("Site", "Elevation", "Taxon", "Individual_number", "Leaf_number")) %>% 
+setdiff(trait2015$Taxon_FoC_corrected, Newleafarea2015$Taxon)
+setdiff(Newleafarea2015$Taxon, trait2015$Taxon_FoC_corrected)
   
   
-  
-  
-
-ggplot(aes(x = Dry_Mass_g, y = LeafArea2, colour = Site)) + 
-  geom_point() +   
-  geom_abline(intercept = 0, slope = 1, colour = "red") +
-  scale_x_log10() + 
-  scale_y_log10()
