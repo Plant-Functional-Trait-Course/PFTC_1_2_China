a459815b6d3585bb5af9125b45ea35b37070e94c
audhalbritter <me@audhalbritter.com>
2017-07-26 23:50:48 +0200

    2015 dry mass wet < dry dry vs area sla

--- Check data with plotting.R
+++ Check data with plotting.R
@@ -1,30 +1,57 @@
-## Check data with some plots
-# wet vs dry
-# colour: Wet_Mass_g < 0.0005
+#### Check data with some plots ####
+
+
+# Dry_Mass_2015 vs Dry_Mass_2016
+Eearlytrait2015 %>% filter(year(ymd(Date)) == "2015") %>%
+  ggplot(aes(x = Dry_Mass_g, y = Dry_Mass_2016_g)) + 
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
+  scale_x_log10() + 
+  scale_y_log10()
+  
+
+## WET VS. DRY MASS
+# colour Tiny wet mass: Wet_Mass_g < 0.0005
 traits %>% mutate(year = as.factor(year(Date))) %>%
-  ggplot(aes(x = Wet_Mass_g, y = Dry_Mass_g, colour = allComments)) + 
+  ggplot(aes(x = Wet_Mass_g, y = Dry_Mass_g, colour = Wet_Mass_g < 0.0005)) + 
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
+  scale_x_log10() + 
+  scale_y_log10() + 
+  facet_wrap(~ year)
+
+
+
+## DRY VS. AREA  
+traits %>% mutate(year = as.factor(year(Date))) %>%
+  ggplot(aes(x = Dry_Mass_g, y = Leaf_Area_cm2, color = AreaFlag)) + 
   geom_point() +   
   geom_abline(intercept = 0, slope = 1, colour = "red") +
   scale_x_log10() + 
   scale_y_log10() + 
   facet_wrap(~ year)
 
-# dry vs area  
-traits %>% mutate(year = as.factor(year(Date))) %>%
-  filter(year == 2015) %>% 
-  ggplot(aes(x = Dry_Mass_g, y = Leaf_Area_cm2, color = allComments)) + 
-  geom_point() +   
-  geom_abline(intercept = 0, slope = 1, colour = "red") +
+
+## SLA
+traits %>% 
+  mutate(year = year(Date)) %>% 
+  arrange(SLA_cm2_g) %>%
+  mutate(flag = cut(SLA_cm2_g, breaks = c(0, 5, 500, Inf))) %>% 
+  ggplot(aes(x = Leaf_Area_cm2, y = Dry_Mass_g, colour = AreaFlag)) + 
+  geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
   scale_x_log10() + 
   scale_y_log10() + 
   facet_wrap(~ year)
 
 
-# large resids
-LargeResid %>% mutate(year = as.factor(year(Date))) %>%
-  ggplot(aes(x = Dry_Mass_g, y = Leaf_Area_cm2, color = abs(resid_Area_Dry) > 1.2)) + 
-  geom_point() +   
-  geom_abline(intercept = 0, slope = 1, colour = "red") +
+traits %>% 
+  mutate(year = year(Date)) %>% 
+  arrange(SLA_cm2_g) %>%
+  mutate(flag = cut(SLA_cm2_g, breaks = c(0, 5, 500, Inf))) %>% 
+  ggplot(aes(x = Leaf_Area_cm2, y = Dry_Mass_g, colour = SLA_cm2_g > 500)) + 
+  geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
   scale_x_log10() + 
   scale_y_log10() + 
   facet_wrap(~ year)
@@ -47,18 +74,14 @@ traits %>% group_by(Taxon) %>% filter(n() > 100) %>%
   facet_grid(Taxon ~ Site, scales = "free")
 
 
-traits %>% 
-  mutate(year = year(Date)) %>% 
-  arrange(SLA_cm2_g) %>%
-  ggplot(aes(x = Wet_Mass_g, y = Dry_Mass_g, colour = SLA_cm2_g > 500)) + 
-  geom_point() + 
-  geom_abline(slope = 1, intercept = 0) + 
+
+# large resids
+LargeResid %>% mutate(year = as.factor(year(Date))) %>%
+  ggplot(aes(x = Dry_Mass_g, y = Leaf_Area_cm2, color = abs(resid_Area_Dry) > 1.2)) + 
+  geom_point() +   
+  geom_abline(intercept = 0, slope = 1, colour = "red") +
   scale_x_log10() + 
   scale_y_log10() + 
   facet_wrap(~ year)
-
-
-
-
 
 
