4e2c06981ffafc464480923b72ed03d1c8125bb1
richardjtelford <richard.telford@bio.uib.no>
2017-09-22 10:20:25 +0200

    Figures for Yan's ms

--- community/Yans_ms.R
+++ community/Yans_ms.R
@@ -4,13 +4,19 @@
 #Effect of temperature treatments (gradient/transplant/OTC) on different responses (richness, evenness, grass/herb ratio. Change in effect over time
 
 ##load packages
+library("tidyverse")
 library("vegan")
 library("ggvegan")
 library("lubridate")
+library("gridExtra")
+
+
+## ---- setup
+source("yans_ms_functions.R")
 
 ##load data
 #climate
-load("climate/climate_month.Rdata")
+load("../climate/climate_month.Rdata")
 summer_temp <- climate_month %>% 
   filter(variable == "Tair") %>% 
   mutate(month = month(month)) %>%
@@ -25,18 +31,28 @@ summer_temp <- climate_month %>%
 #Warm1 = TairD - TairO  
 
 pred_temp <- bind_rows(
-  summer_temp %>% filter(logger == "gradient") %>% mutate(TTtreat = "control"),#control
-  summer_temp %>% filter(logger == "gradient") %>% mutate(TTtreat = "local"),#local transplant (== control)
-  summer_temp %>% spread(key = logger, value = mean) %>% mutate(mean = otc - gradient, TTtreat = "OTC") %>% select(-gradient, -otc),
-  summer_temp %>% filter(logger == "gradient") %>% mutate(mean = lead(mean) - mean, TTtreat = "warm1") %>% filter(site != "L")
-)  %>% select(-logger) %>%
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(TTtreat = "control"),#control
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(TTtreat = "local"),#local transplant (== control)
+  summer_temp %>% 
+    spread(key = logger, value = mean) %>% 
+    mutate(mean = otc - gradient, TTtreat = "OTC") %>% 
+    select(-gradient, -otc),
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(mean = lead(mean) - mean, TTtreat = "warm1") %>% 
+    filter(site != "L")
+)  %>% 
+  select(-logger) %>%
   mutate(mean = if_else(site == "L" & TTtreat == "OTC", 2, mean))
-warning("using estiated OTC effect for site L")
+warning("using estimated OTC effect for site L")
 
 
 #community
-source("community/start_here.R")
-#source("community/ordinations.R")
+source("start_here.R")
 cover_thin <- cover_thin %>% 
   filter(TTtreat %in% c("control", "local", "warm1", "OTC"))
 
@@ -44,9 +60,14 @@ cover_thin <- cover_thin %>%
 
 #turf environment
 
+## functional groups
+fun_gp <- tbl(con, "taxon") %>% 
+  select(species, functionalGroup) %>% 
+  collect()
 
+fun_gp %>% count(functionalGroup)
 
-## ordination
+## ---- ordination
 cover_fat <- cover_thin %>% 
   select(-speciesName) %>% 
   arrange(year) %>%
@@ -64,51 +85,65 @@ fNMDS <- fortify(NMDS) %>%
 
 treat_colours <- c("black", "grey50", "red", "green")
 
-g <-ggplot(fNMDS, aes(x = Dim1, y = Dim2, shape = originSiteID, colour = TTtreat, group = originPlotID, fill = TTtreat)) +
+g <- ggplot(fNMDS, aes(x = Dim1, y = Dim2, shape = originSiteID, colour = TTtreat, group = originPlotID, fill = TTtreat)) +
   geom_point(aes(size = ifelse(year == min(year), "First", "Other"))) +
   geom_path() + 
-  coord_fixed(ratio = 1) +
-  scale_size_discrete(range = c(1, 3), limits = c("Other", "First"), breaks = c("First", "Other")) +
-  scale_colour_manual(values = treat_colours) +
-  scale_fill_manual(values = treat_colours) +
-  scale_shape_manual(values = c(24, 22, 23, 25)) +
+  coord_equal() +
+  scale_size_discrete(range = c(1, 2.5), limits = c("Other", "First"), breaks = c("First", "Other")) +
+  scale_colour_manual(values = treat_colours, limits = levels(cover_fat$TTtreat)) +
+  scale_fill_manual(values = treat_colours, limits = levels(cover_fat$TTtreat)) +
+  scale_shape_manual(values = c(24, 22, 23, 25), limits = levels(cover_fat$originSiteID)) +
   guides(shape = guide_legend(override.aes = list(fill = "black"))) +
   labs(x = "NMDS 1", y = "NMDS 2", colour = "Treatment", fill = "Treatment", shape = "Site", size = "Year")
-g
+#g
 
 
+ 
+HA <- two_sites_nmds("H", "A")
+AM <- two_sites_nmds("A", "M")
+ML <- two_sites_nmds("M", "L")
+LM <- two_sites_nmds("L", "M")
 
-## responses
-#richness
-richnessC <- cover_thin %>% 
-  group_by(turfID, originBlockID, TTtreat, originSiteID, year) %>%   summarise(n = n()) %>% 
+
+gg <- ggplotGrob(g)$grobs
+legend <- gg[[which(sapply(gg, function(x) x$name) == "guide-box")]]
+g <- g + theme(legend.position = "none", axis.title = element_blank())
+grid.arrange(g %+% HA + ggtitle("H - A"),
+             g %+% AM + ggtitle("A - M"),
+             g %+% ML + ggtitle("M - L"),
+             g %+% LM + ggtitle("L - "),
+             legend,
+             layout_matrix = rbind(c(1, 2, 5), c(3, 4, 5)), widths = c(.4, .4, .2),
+             bottom = "NMDS1", left = "NMDS2")
+
+
+## ---- responses
+responses <- cover_thin %>% 
+  left_join(fun_gp) %>% 
+  group_by(turfID, originBlockID, TTtreat, originSiteID, year) %>%  
+  summarise(richness = n(), 
+            diversity = diversity(cover), 
+            N1 = exp(diversity),
+            evenness = diversity/log(richness),
+            sumCover = sum(cover),
+            propGraminoid = sum(cover[functionalGroup %in% c("gramineae", "sedge")])/sumCover
+            ) %>% 
   group_by(originBlockID, TTtreat, originSiteID, year) %>%
   select(-turfID) %>% 
-  full_join(pred_temp, by = c("originSiteID" = "site", "TTtreat" = "TTtreat")) %>%
-  mutate(contrast = if_else(TTtreat %in% c("local", "control"), 0, mean))
+  full_join(pred_temp, by = c("originSiteID" = "site", "TTtreat" = "TTtreat")) %>% 
+  ungroup() %>% 
+  mutate(contrast = if_else(TTtreat %in% c("local", "control"), 0, mean), 
+         TTtreat = factor(TTtreat, levels = c("control", "local", "warm1", "OTC"))) 
 
-gc <- ggplot(richnessC %>% filter(TTtreat %in% c("local", "control"), year == 2016), aes(x = mean, y = n, colour = originSiteID, shape = TTtreat, group = 1)) + 
-  geom_jitter(height = 0, width = 0.1) +
-  geom_smooth(method = "lm") +
-  scale_shape(limits = unique(richnessC$TTtreat)[c(2,3,1,4)]) +
-  labs(x = "Temperature °C", y = "Species Richness", colour = "Site", shape = "Treatment") +
-  ggtitle("Gradient")
+plot_three_treatments(responses, column = "richness", ylab = "Species Richness")
+plot_three_treatments(responses, column = "evenness", ylab = "Species Evenness")
+plot_three_treatments(responses, column = "diversity", ylab = "Diversity")
+plot_three_treatments(responses, column = "sumCover", ylab = "Sum of Covers")
+plot_three_treatments(responses, column = "propGraminoid", ylab = "Proportional Graminoids")
 
-gw <- gc %+% (richnessC %>% filter(TTtreat %in% c("local", "warm1"), year == 2016)) + aes(x = contrast, group = originSiteID) +
-  ggtitle("Transplant") + theme(legend.position = "none") + labs(x = "Temperature Contrast °C")
-
-go <- gw %+% (richnessC %>% filter(TTtreat %in% c("control", "OTC"), year == 2016)) + 
-  ggtitle("OTC")
-         
-library("grid")
-library("gridExtra")
-grid.arrange(gc, gw, go, layout_matrix = rbind(c(1, 1), c(2, 3)))
-
-
-# more responses
 # table of effects
 
-
+## ---- other
 
 ## turnover on gradient
 turnover <- data_frame(
---
+++ community/yans_ms_functions.R
@@ -0,0 +1,45 @@
+plot_three_treatments <- function(data, column, ylab = "")  {
+    gc <- ggplot(data %>% filter(TTtreat %in% c("local", "control"), year == 2016), 
+                 aes_string(x = "mean", y = column, colour = "originSiteID", shape = "TTtreat", group = 1)) + 
+    geom_jitter(height = 0, width = 0.1) +
+    geom_smooth(method = "lm") +
+    scale_shape(limits = levels(data$TTtreat)) +
+    labs(x = "Temperature °C", y = ylab, colour = "Site", shape = "Treatment") +
+    ggtitle("Gradient")
+  
+  gw <- gc %+% (data %>% filter(TTtreat %in% c("local", "warm1"), year == 2016)) + 
+    aes(x = contrast, group = originSiteID) +
+    ggtitle("Transplant") + 
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 3)) +
+    theme(legend.position = "none") + 
+    labs(x = "Temperature Contrast °C")
+  
+  
+  go <- gw %+% (data %>% filter(TTtreat %in% c("control", "OTC"), year == 2016)) + 
+    ggtitle("OTC")
+  
+  g <- ggplotGrob(gc)$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  grid.arrange(gc + theme(legend.position = "none"), gw, go,legend, layout_matrix = rbind(c(1, 1, 4), c(2, 3, 4)), widths = c(.4, .4, .2))
+}
+
+#nmds of two sites
+two_sites_nmds <- function(site1, site2){
+  cover_fat <- cover_fat %>% 
+    filter(originSiteID == site1 | (originSiteID == site2 & TTtreat  %in% c("control", "local")))
+  
+  cover_fat_spp <- cover_fat %>% 
+    select(-(originSiteID:year))
+  
+  NMDS <- metaMDS(cover_fat_spp, noshare = TRUE, try = 30)#DNC
+  
+  fNMDS <- fortify(NMDS) %>% 
+    filter(Score == "sites") %>%
+    bind_cols(cover_fat %>% select(originSiteID:year))
+  
+  fNMDS
+}
+
+
+
+
