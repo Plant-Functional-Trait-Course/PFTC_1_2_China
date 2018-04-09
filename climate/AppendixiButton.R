6b2a175aa01a2bb1c9ac6c3c12df3faafbc91bfc
audhalbritter <audhalbritter.com>
2018-04-09 20:04:35 +0200

    calculate monthly air temp difference between treatment and control

--- climate/AppendixiButton.R
+++ climate/AppendixiButton.R
@@ -30,6 +30,26 @@ monthlyiButton %>%
   facet_grid(Temperature ~ destSite, scales = "free") +
   theme_minimal()
 
+monthlyTemps <- monthlyiButton %>% 
+  filter(depth == "air") %>% 
+  mutate(destSite = site) %>% 
+  rename(origSite = site) %>% 
+  bind_rows(WarmAirT) %>% 
+  select(-Tmin, -Tmax, -origSite, -depth) %>%
+  unite(united, Tmean, Tse, sep = "_") %>% 
+  spread(key = treatment, value = united) %>% 
+  separate(col = C, into = c("Control_mean", "Control_se"), sep = "_", convert = TRUE) %>%
+  separate(col = OTC, into = c("OTC_mean", "OTC_se"), sep = "_", convert = TRUE) %>%
+  mutate(Transplant = ifelse(is.na(Transplant), "NA_NA", Transplant)) %>% 
+  separate(col = Transplant, into = c("Transplant_mean", "Transplant_se"), sep = "_", convert = TRUE) %>%
+  mutate(OTC_mean = OTC_mean -  Control_mean, Transplant_mean = Transplant_mean - Control_mean) %>% 
+  mutate(OTC_se = sqrt(Control_se^2 + OTC_se^2), Transplant_se = sqrt(Control_se^2 + Transplant_se^2)) %>% 
+  select(-Control_mean, -Control_se) %>% 
+  mutate(destSite = factor(destSite, levels = c("H", "A", "M", "L")))
+write.csv(monthlyTemps, "monthlyTemps.csv")
+
+
+
 # create Transplant values (take controls from one step down)
 WarmAirTDayily <- dailyiButton %>% 
   filter(depth == "air", treatment == "C", site != "H") %>%
