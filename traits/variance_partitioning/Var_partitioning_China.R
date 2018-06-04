c20dc1d1961f878fbd03fa0f1c23335dfa4e9b29
lorahpatterson <lorahbeth@gmail.com>
2018-06-04 11:38:11 -0600

    Update with latest trait data; log transformation

--- traits/variance_partitioning/Var_partitioning_China.R
+++ traits/variance_partitioning/Var_partitioning_China.R
@@ -1,6 +1,7 @@
 ###########################################
-# 4-4-17
-# Lorah Patterson - Variance partitioning of 2016 China leaf trait data
+#6-4-18
+# Lorah Patterson - Variance partitioning of 2015 and 2016 China leaf trait data
+
 
 library(tidyverse)
 library(taxize)
@@ -10,20 +11,22 @@ library(jsonlite)
 library(ape)
 library(BIEN)
 library(nlme)
+library(ggplot2)
 
-# Load 2015 and 2016 China trait data files and merge them using trait_2017_analysis.R in transplant github repository.
-source("traits/trait_2017_analysis.R")
+#Get most recent trait data from dropbox (as of June 2018, most recent data was uploaded in January 2018)
+#setwd("~/Documents/R_data/China_2016_partitioning/012018_data_update/")
 
-### Subset data to only contain trait data collected in 2016 (because as of April 2017, 2015 trait data needs further error correction) ###
-traits$year <- year(traits$Date) # make a column containing the year
-varpart2016 <- subset(traits, traits$year=="2016") # subset so that only 2016 trait data remains
+load("traits.RData") #the data frame is called "traits"
 
-### Get a column with names of genera from trait data ###
-specgen <- strsplit(varpart2016$Taxon," ") # split the specific epithet into genus and species
+###############################################################
+##### Add taxonomic data to trait data
+
+#Get a column with names of genera from trait data
+specgen <- strsplit(traits$Taxon," ") # split the specific epithet into genus and species
 specgen$genus <- as.character(sapply(specgen, head, 1)) # get only the genera data out so that the next step to extract plant family is quicker and matches easier will less NAs
-varpart2016$genus <- specgen$genus # add genus column to trait data
- 
-### Get family and order from genus using BIEN ###
+traits$genus <- specgen$genus # add genus column to trait data
+
+#Get family and order from genus using BIEN
 taxspecgen<-BIEN_taxonomy_genus(genus = specgen$genus)
 taxspecgen2 <- taxspecgen %>% select(order, scrubbed_family, scrubbed_genus) # select order, family, genus columns only
 taxspecgen2$genus <- taxspecgen2$scrubbed_genus # rename genus column
@@ -33,138 +36,118 @@ taxspecgen4 <- distinct(taxspecgen3) # Remove duplicate rows
 # The results of this specify two families for Carex and Athyrium. Delete the incorrect rows. CAUTION: You may have to modify this if using different data.
 taxspecgen5 <- subset(taxspecgen4, family!="Athyriaceae" & family!="Unknown")
 
-### Join trait data to BIEN taxonomic data ###
-varpart2016join <- left_join(taxspecgen5, varpart2016, by = "genus")
-# There are still some species naming errors in here, which make it seem like the join resulted in lost data. Will be fixed in the future.
+#Join trait data to BIEN taxonomic data
+varpart2016join <- full_join(taxspecgen5, traits, by = "genus")
 
-### Change letters representing sites (L,M,H,A) to real names (Lower, Middle, High, Alpine) ###
+#Change letters representing sites (L,M,H,A) to real names (Lower, Middle, High, Alpine)
 levels(varpart2016join$Site) <- c(levels(varpart2016join$Site), "lower",
-                                 "middle", "high", "alpine")
+                                  "middle", "high", "alpine")
 
 varpart2016join$Site[varpart2016join$Site == "L"] <- "lower"
 varpart2016join$Site[varpart2016join$Site == "M"] <- "middle"
 varpart2016join$Site[varpart2016join$Site == "H"] <- "high"
 varpart2016join$Site[varpart2016join$Site == "A"] <- "alpine"
 
-### Rename column name "taxon" to "species" ###
+#Rename column name "taxon" to "species"
 varpart2016join$species <- varpart2016join$Taxon
 
+###############################################################
+### Clean data
 
-### Clean data errors. ###
-varpart2016join$LDMC[varpart2016join$Wet_Mass_g <= varpart2016join$Dry_Mass_g] <- NA # when there is an error that wet mass is less than or equals dry mass, change the LDMC value to NA so that it is not considered in the analysis
+# Data I imported into this was already cleaned by Aud and Richard, so I just have to remove all flag rows that have been flagged #zap (which signifies an error that can't be corrected in the data. See file:///Users/lorahpatterson/Documents/R_data/China_2016_partitioning/102017_data_updates/Data_Cleaning_documentation.html). This leaves some data flagged with comments that are warnings.
 
-varpart2016join$SLA_cm2_g[varpart2016join$Wet_Mass_g <= varpart2016join$Dry_Mass_g] <- NA # when there is an error that wet mass is less than or equals dry mass, change the SLA value to NA so that it is not considered in the analysis
+traitsclean <- filter(varpart2016join, !grepl("#zap", AreaFlag)) %>% filter(!grepl("#zap", WetFlag)) %>% filter(!grepl("#zap", DryFlag)) %>% filter(!grepl("#zap", ThickFlag)) %>% filter(!grepl("#zap", GeneralFlag))
 
-varpart2016join$SLA_cm2_g[varpart2016join$SLA_cm2_g >500] <- NA # when there is an error that SLA is greater than 500, change the SLA value to NA so that it is not considered in the analysis
-
-varpart2016join$SLA_cm2_g[varpart2016join$SLA_cm2_g <5] <- NA # when there is an error that SLA is less than 5, change the SLA value to NA so that it is not considered in the analysis
-
-
-
+write.csv(traitsclean, "traitsclean.csv")
 ### If necessary, remove gymnosperms and equisetum from data set so that we are only analyzing data for angiosperms ###
-# varpart2016filter <- filter(varpart2016join, varpart2016join$family!= "Pinaceae", varpart2016join$family!="Cupressaceae", varpart2016join$family!= "Equisetaceae")
+traitsangio <- traitsclean %>% filter(!family %in% c("Pinaceae", "Cupressaceae", "Equisetaceae"))
+
+#write.csv(traitsangio, "traitsangio.csv")
+
+### Subset by only those leaves collected in project=LOCAL (i.e. gradient outside the fence, grazed) or project=C or 0 (control plots within the fence, ungrazed) ###
+varpart <- filter(traitsangio,Project=="LOCAL" | Project=="C" | Project=="0" | is.na(Project))
+
+#write this to file to use in R markdown so that you don't need to create cleaned file again.
+save(varpart, file= "varpart.RData")
+
+# Subset varpart into different objects based on leaf collection location: local or control.
+varpartLocal <- subset(traitsangio, Project=="LOCAL") # only leaves outside fence, grazed
+varpartControl <- subset(traitsangio, Project=="C") # only leaves inside fence, ungrazed
+
+varpartLower <- subset(varpart, Site=="lower")
+varpartMiddle <- subset(varpart, Site=="middle")
+varpartAlpine <- subset(varpart, Site=="alpine")
+varpartHigh <- subset(varpart, Site="high")
+
+###############################################################
+### Variance partitioning analysis
+
+# Variance partitioning by taxonomic level of all non-experimental leaves collected in 2015 and 2016 inside and outside fence (i.e., in Control and Local projects). Site represents the within population variance. 
+
+#load the cleaned traits data you created above
+load("varpart.RData")
+
+# Without log transformation
+vchinatraitsSLAsite <- varcomp(lme(SLA_cm2_g~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsAreasite <- varcomp(lme(Leaf_Area_cm2~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsThicksite <- varcomp(lme(Leaf_Thickness_Ave_mm~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsLDMCsite <- varcomp(lme(LDMC~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+
+vchinatraitsSLAsite
+vchinatraitsLDMCsite
+vchinatraitsThicksite
+vchinatraitsAreasite
+
+# With log transformation
+vchinatraitsSLAlog <- varcomp(lme(log(SLA_cm2_g)~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsArealog <- varcomp(lme(log(Leaf_Area_cm2)~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsThicklog <- varcomp(lme(log(Leaf_Thickness_Ave_mm)~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+vchinatraitsLDMClog <- varcomp(lme(log(LDMC)~1, random=~1|order/family/genus/species/Site, data=varpart, na.action = na.omit), 1)
+
+vchinatraitsSLAlog
+vchinatraitsLDMClog
+vchinatraitsThicklog
+vchinatraitsArealog
+
+###############################################################
+### Plotting variance partitioning (using all log transformed results, except for with SLA)
 
 
+vchinatraitsSLAsite2<- data.frame(as.list(vchinatraitsSLAsite)) # turns vector into data frame
+vchinatraitsSLAsite2$trait <- "SLA" # add a column called trait with SLA
 
-### Subset by only those leaves collected in project=LOCAL (i.e. gradient outside the fence, grazed) or project=C (control plots within the fence, ungrazed) ###
-varpart <- subset(varpart2016join, Project=="LOCAL" | Project=="C")
+vchinatraitsLDMClog2 <- data.frame(as.list(vchinatraitsLDMClog)) # turns vector into data frame
+vchinatraitsLDMClog2$trait <- "LDMC" # add a column called trait with LDMC
 
-varpartLocal <- subset(varpart2016join, Project=="LOCAL") # only leaves outside fence, grazed
-varpartControl <- subset(varpart2016join, Project=="C") # only leaves inside fence, ungrazed
-##############################################################
-### lme and varcomp analysis ###
+vchinatraitsThicklog2 <- data.frame(as.list(vchinatraitsThicklog)) # turns vector into data frame
+vchinatraitsThicklog2$trait <- "LT" # add a column called trait with leaf thickness (LT)
 
-#bootvchinatraitsSLA <- boot(data=varpart, vchinatraitsSLA, R=5, statistic=)
-
-### Variance partitioning by taxonomic level: order, family, genus, species, within species ###
-vchinatraitsSLA <- varcomp(lme(SLA_cm2_g~1, random=~1|order/family/genus/species, data=varpart, na.action = na.omit), 1)
-vchinatraitsArea <- varcomp(lme(Leaf_Area_cm2~1, random=~1|order/family/genus/species, data=varpart, na.action = na.omit), 1)
-vchinatraitsThick <- varcomp(lme(Leaf_Thickness_Ave_mm~1, random=~1|order/family/genus/species, data=varpart, na.action = na.omit), 1)
-vchinatraitsLDMC <- varcomp(lme(LDMC~1, random=~1|order/family/genus/species, data=varpart, na.action = na.omit), 1)
-
-# show results
-vchinatraitsSLA
-vchinatraitsArea
-vchinatraitsThick
-vchinatraitsLDMC
-
-### Variance partitioning of ONLY project=LOCAL leaves (i.e. gradient outside the fence, grazed) then ONLY project=C (control plots within the fence, ungrazed) to see if there is any difference ###
-vchinatraitsSLALocal <- varcomp(lme(SLA_cm2_g~1, random=~1|order/family/genus/species, data=varpartLocal, na.action = na.omit), 1)
-vchinatraitsAreaLocal <- varcomp(lme(Leaf_Area_cm2~1, random=~1|order/family/genus/species, data=varpartLocal, na.action = na.omit), 1)
-vchinatraitsThickLocal <- varcomp(lme(Leaf_Thickness_Ave_mm~1, random=~1|order/family/genus/species, data=varpartLocal, na.action = na.omit), 1)
-vchinatraitsLDMCLocal <- varcomp(lme(LDMC~1, random=~1|order/family/genus/species, data=varpartLocal, na.action = na.omit), 1)
-# show results
-vchinatraitsSLALocal
-vchinatraitsAreaLocal
-vchinatraitsThickLocal
-vchinatraitsLDMCLocal
-
-vchinatraitsSLAControl <- varcomp(lme(SLA_cm2_g~1, random=~1|order/family/genus/species, data=varpartControl, na.action = na.omit), 1)
-vchinatraitsAreaControl <- varcomp(lme(Leaf_Area_cm2~1, random=~1|order/family/genus/species, data=varpartControl, na.action = na.omit), 1)
-vchinatraitsThickControl <- varcomp(lme(Leaf_Thickness_Ave_mm~1, random=~1|order/family/genus/species, data=varpartControl, na.action = na.omit), 1)
-vchinatraitsLDMCControl <- varcomp(lme(LDMC~1, random=~1|order/family/genus/species, data=varpartControl, na.action = na.omit), 1)
-# show results
-vchinatraitsSLAControl
-vchinatraitsAreaControl
-vchinatraitsThickControl
-vchinatraitsLDMCControl
-
-#####################
-### Variance partitioning by spatial level ###
-vchinatraitsSiteSLA <- varcomp(lme(SLA_cm2.g~1, random=~1|Site, data=chinatraitsfinal, na.action = na.omit), 1)
-
-vchinatraitsSiteArea <- varcomp(lme(Leaf_Area_cm2~1, random=~1|Site, data=chinatraitsfinal, na.action = na.omit), 1)
-
-vchinatraitsSiteThick <- varcomp(lme(Leaf_Thickness_Ave_mm~1, random=~1|Site, data=chinatraitsfinal, na.action = na.omit), 1)
-
-vchinatraitsSiteLDMC <- varcomp(lme(LDMC~1, random=~1|Site, data=chinatraitsfinal, na.action = na.omit), 1)
-
-# show results
-vchinatraitsSiteSLA
-vchinatraitsSiteArea
-vchinatraitsSiteThick
-vchinatraitsSiteLDMC
-
-#####################################################
-### PLOTS ###
-
-### Stacked bar graph of variance partitioning: taxonomic levels ###
-
-vchinatraitsSLA2 <- data.frame(as.list(vchinatraitsSLA)) # turns vector into data frame
-vchinatraitsSLA2$trait <- "SLA" # add a column called trait with SLA
-
-vchinatraitsLDMC2 <- data.frame(as.list(vchinatraitsLDMC)) # turns vector into data frame
-vchinatraitsLDMC2$trait <- "LDMC" # add a column called trait with LDMC
-
-vchinatraitsThick2 <- data.frame(as.list(vchinatraitsThick)) # turns vector into data frame
-vchinatraitsThick2$trait <- "Thick" # add a column called trait with LDMC
-
-vchinatraitsArea2 <- data.frame(as.list(vchinatraitsArea)) # turns vector into data frame
-vchinatraitsArea2$trait <- "Area" # add a column called trait with LDMC
+vchinatraitsArealog2<- data.frame(as.list(vchinatraitsArealog)) # turns vector into data frame
+vchinatraitsArealog2$trait <- "LA" # add a column called trait with leaf area (LA)
 
 # Mutate the wide data frames into long data frames.
-vchinatraitsSLA3 <- gather(vchinatraitsSLA2, "Taxonomic.level", "Percent.variance", 1:5)
-vchinatraitsLDMC3 <- gather(vchinatraitsLDMC2, "Taxonomic.level", "Percent.variance", 1:5)
-vchinatraitsThick3 <-  gather(vchinatraitsThick2, "Taxonomic.level", "Percent.variance", 1:5)
-vchinatraitsArea3 <-  gather(vchinatraitsArea2, "Taxonomic.level", "Percent.variance", 1:5)
+vchinatraitsSLAsite3<- gather(vchinatraitsSLAsite2, "Level", "Percent.variance", 1:6)
+vchinatraitsLDMClog3 <- gather(vchinatraitsLDMClog2, "Level", "Percent.variance", 1:6)
+vchinatraitsThicklog3 <-  gather(vchinatraitsThicklog2, "Level", "Percent.variance", 1:6)
+vchinatraitsArealog3 <-  gather(vchinatraitsArealog2, "Level", "Percent.variance", 1:6)
 
 # Bind all the data frames
-varcompanalysis <- bind_rows(vchinatraitsSLA3, vchinatraitsLDMC3, vchinatraitsThick3, vchinatraitsArea3)
-varcompanalysis$Percent.variance.100 <- varcompanalysis$Percent.variance*100 # add a new column where Percent.variance is multiplied by 100 to get actual percent and not decimal.
+varcompanalysislog <- bind_rows(vchinatraitsSLAsite3, vchinatraitsLDMClog3, vchinatraitsThicklog3, vchinatraitsArealog3)
+varcompanalysislog$Percent.variance.100 <- varcompanalysislog$Percent.variance*100 # add a new column where Percent.variance is multiplied by 100 to get actual percent and not decimal.
 
 # Change "Within" to "within species"
-levels(varcompanalysis$Taxonomic.level) <- c(levels(varcompanalysis$Taxonomic.level), "within species")
-varcompanalysis$Taxonomic.level[varcompanalysis$Taxonomic.level=="Within"] <- "within species"
+levels(varcompanalysislog$Level) <- c(levels(varcompanalysislog$Level), "within species")
+varcompanalysislog$Level[varcompanalysislog$Level=="Within"] <- "within species"
+
+# Change "Site" to "site"
+levels(varcompanalysislog$Level) <- c(levels(varcompanalysislog$Level), "site")
+varcompanalysislog$Level[varcompanalysislog$Level=="Site"] <- "site"
 
 # Change the order of the taxonomic levels
-varcompanalysis$Taxonomic.level <- factor(varcompanalysis$Taxonomic.level, levels= c("order", "family", "genus", "species", "within species"))
+varcompanalysislog$Level <- factor(varcompanalysislog$Level, levels= c("order", "family", "genus", "species", "site", "within species"))
 
 # Change the order of the traits
-varcompanalysis$trait <- factor(varcompanalysis$trait, levels= c ("SLA", "LDMC", "Thick", "Area"))
+varcompanalysislog$trait <- factor(varcompanalysislog$trait, levels= c ("SLA", "LDMC", "LT", "LA"))
 
 # Make the stacked bar graph
-ggplot(data = varcompanalysis, aes(x = trait, y = Percent.variance.100, fill = Taxonomic.level)) + geom_bar(stat="identity") + labs(title = "Variance Analysis", x= "Leaf trait" , y = "Percent variance" , fill="Taxonomic level") + scale_fill_manual(values=c("#003366", "#0066CC", "#3399FF", "#99CCFF", "#FF9900")) + theme(text = element_text(size=20))
-
-
-### Stacked bar graph of variance partitioning: spatial levels ###
-
+ggplot(data = varcompanalysislog, aes(x = trait, y = Percent.variance.100, fill = Level)) + geom_bar(stat="identity") + labs(x= "Leaf trait" , y = "Percent variance" , fill="Level") + scale_fill_manual(values=c("#003366", "#0066CC", "#3399FF", "#99CCFF", "#000000", "#FF9900")) + theme(text = element_text(size=20)) + theme_classic() #+ggtitle("Using log transformed data")
