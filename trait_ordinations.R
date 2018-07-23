4e36d558ee11691525d150091ec838d43956067a
richardjtelford <richard.telford@bio.uib.no>
2018-07-23 21:09:44 +0200

    first pass at trait ordinations

---
+++ trait_ordinations.R
@@ -0,0 +1,79 @@
+library("tidyverse")
+library("vegan")
+library("ggvegan")
+
+cwm <- readRDS("trait_distributions/data/cwm_output_china.rds") %>% 
+  as_tibble() %>% 
+  mutate(mean = as.numeric(as.character(mean))) %>% #why is mean a factor???
+  mutate(year = as.numeric(year)) %>% 
+  mutate(site = factor(site, levels = c("H", "A", "M", "L"))) %>% 
+  rename(TTtreat = treatment) %>% 
+  mutate(TTtreat = factor(TTtreat, levels = c("C", "O", 1:4, "OTC"), labels = c("Control", "Local", "Warm1", "Cool1", "Warm3", "Cool3", "OTC")))
+
+#histograms
+cwm %>% 
+  filter(TTtreat %in% c("Control", "Local")) %>% 
+  ggplot(aes(x = mean, fill = site)) +
+  geom_histogram() +
+  facet_wrap(~ trait, scales = "free_x")
+
+cwm %>% 
+  filter(TTtreat %in% c("OTC")) %>% 
+  ggplot(aes(x = mean, fill = site)) +
+  geom_histogram() +
+  facet_wrap(~ trait, scales = "free_x")
+
+
+cwm %>% 
+  filter(TTtreat %in% c("Warm1")) %>% 
+  ggplot(aes(x = mean, fill = site)) +
+  geom_histogram() +
+  facet_wrap(~ trait, scales = "free_x")
+
+cwm %>% 
+  filter(TTtreat %in% c("Cool1")) %>% 
+  ggplot(aes(x = mean, fill = site)) +
+  geom_histogram() +
+  facet_wrap(~ trait, scales = "free_x")
+
+### ordinations
+cwm_fat <- cwm %>% spread(key = trait, value = mean) 
+
+## control plots
+controls <- cwm_fat %>% filter(TTtreat %in% c("Control", "Local"))
+
+controls_pca <- rda(controls %>% select(-(turf:year)), scale = TRUE)
+
+control_sites <- bind_cols(
+  controls %>% select(turf:year), 
+  fortify(controls_pca, display = "sites"))
+
+control_traits <- fortify(controls_pca, display = "species")
+
+treat_colours <- c("black", "grey50", "pink", "lightgreen", "red", "green", "orange")
+
+ggplot(control_sites, aes(x = PC1, y = PC2, shape = site, colour = TTtreat, fill = TTtreat, group = turf)) +  
+  geom_point(aes(size = ifelse(year == min(year), "First", "Other"))) +
+  geom_path() +
+  #arrows
+  geom_segment(data = control_traits, 
+               aes(x = 0, y = 0, xend = PC1, yend = PC2), 
+               arrow = arrow(length = unit(0.2, "cm")), 
+               colour = "black",
+               inherit.aes = FALSE) +
+  geom_text(data = control_traits, 
+            aes(x = PC1 * 1.1,y = PC2 * 1.1, label = Label), 
+            size = 4,
+            inherit.aes = FALSE) +
+  coord_equal() +
+  scale_size_discrete(range = c(1, 2.5), limits = c("Other", "First"), breaks = c("First", "Other")) +
+  scale_colour_manual(values = treat_colours, limits = levels(cwm_fat$TTtreat)) +
+  scale_fill_manual(values = treat_colours, limits = levels(cwm_fat$TTtreat)) +
+  scale_shape_manual(values = c(24, 22, 23, 25), limits = levels(cwm_fat$site)) +
+  guides(shape = guide_legend(override.aes = list(fill = "black"))) +
+  labs(x = "NMDS 1", y = "NMDS 2", colour = "Treatment", fill = "Treatment", shape = "Site", size = "Year")
+
+
+
+
+
