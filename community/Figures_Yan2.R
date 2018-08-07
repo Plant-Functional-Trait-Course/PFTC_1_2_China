c9379e6cf38d06d944f6b1829079d0f83fc16c0d
audhalbritter <audhalbritter.com>
2018-08-07 16:13:42 +0200

    move unused stuff to appendix file
    correct models for gradient approach
    
    fix plot

--- community/Figures_Yan2.R
+++ community/Figures_Yan2.R
@@ -62,61 +62,8 @@ fun_gp <- tbl(con, "taxon") %>%
   select(species, functionalGroup) %>% 
   collect()
 
-## ordination
-cover_fat <- cover_thin %>% 
-  select(-speciesName, -flag) %>% 
-  arrange(year) %>%
-  filter(TTtreat %in% c("local", "control", "warm1", "OTC")) %>%
-  spread(key = species, value = cover, fill = 0) %>%
-  mutate(TTtreat = droplevels(TTtreat))
 
-cover_fat_spp <- cover_fat %>% select(-(originSiteID:year))
-
-set.seed(32)
-NMDS <- metaMDS(cover_fat_spp, noshare = TRUE, try = 30)#DNC
-
-fNMDS <- fortify(NMDS) %>% 
-  filter(Score == "sites") %>%
-  bind_cols(cover_fat %>% select(originSiteID:year))
-
-treat_colours <- c("grey", "grey40", "orange", "purple")
-
-g <- ggplot(fNMDS, aes(x = NMDS1, y = NMDS2, shape = originSiteID, colour = TTtreat, group = originPlotID, fill = TTtreat)) +
-  geom_point(aes(size = ifelse(year == min(year), "First", "Other"))) +
-  geom_path() + 
-  coord_equal() +
-  scale_size_discrete(range = c(1, 2.5), limits = c("Other", "First"), breaks = c("First", "Other")) +
-  scale_colour_manual(values = treat_colours, limits = levels(cover_fat$TTtreat), labels=c("Control", "Local transplant", "Transplant", "OTC")) +
-  scale_fill_manual(values = treat_colours, limits = levels(cover_fat$TTtreat), labels=c("Control", "Local transplant", "Transplant", "OTC")) +
-  scale_shape_manual(values = c(24, 22, 23, 25), limits = levels(cover_fat$originSiteID), labels=c("High alpine", "Alpine", "Middle", "Low")) +
-  guides(shape = guide_legend(override.aes = list(fill = "black"))) +
-  labs(colour = "Treatment", fill = "Treatment", shape = "Site", size = "Year") 
-
-set.seed(32)
-HA <- two_sites_nmds("H", "A")
-AM <- two_sites_nmds("A", "M")
-ML <- two_sites_nmds("M", "L")
-LM <- two_sites_nmds("L", "M") 
-
-HA <- HA %>% mutate(NMDS1 = -NMDS1)
-ML <- ML %>% mutate(NMDS1 = -NMDS1)
-LM <- LM %>% mutate(NMDS1 = -NMDS1)
-
-
-all_ord <- bind_rows(
-  `H - A` = HA, 
-  `A - M` = AM, 
-  `M - L` = ML, 
-  `L - ` = LM, .id = "which") %>% 
-  mutate(which = factor(which, levels = c("H - A", "A - M", "M - L", "L - ")))
-
-OrdinationPlot <- g %+% all_ord +
-  facet_wrap(~ which)
-
-ggsave(OrdinationPlot, filename = "community/FinalFigures/OrdinationPlot.jpg", height = 7, width = 8, dpi = 300)
-
-
-## responses
+## Calculate responses
 responses <- cover_thin %>% 
   left_join(fun_gp) %>% 
   left_join(turf_env) %>% 
@@ -137,7 +84,7 @@ responses <- cover_thin %>%
          TTtreat = factor(TTtreat, levels = c("control", "local", "warm1", "OTC"))) 
 
 
-
+# make separate data sets for 2016 and different treatments
 Gradient <- responses %>% 
   filter(year == 2016, TTtreat %in% c("local", "control")) 
 OTC <- responses %>% 
@@ -147,29 +94,29 @@ Transplant <- responses %>%
 
 Transplant <- bind_rows(Transplant = Transplant, Gradient = Gradient, OTC = OTC, .id = "experiment")
 
+# function to get aic values
 ## ---- regressions_lines
 augment_aic <- function(x){
   augment(x) %>% 
     mutate(aic = AIC(x))
 }
 
+# compare different models
 gradient <- responses %>% 
   filter(year == 2016, TTtreat %in% c("local", "control")) %>% 
   gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
   filter(!is.na(value)) %>% 
   group_by(variable) %>% 
   # mutate(value = scale(value)) %>% 
-  do(
-    mod3 = lm(value ~ 1 + originSiteID - originSiteID, data = .),
-    mod0 = lm(value ~ originSiteID + mean - mean, data = .),
-    mod1 = lm(value ~ mean + originSiteID, data = .),
-    mod2 = lm(value ~ mean, data = .)
+  do(mod0 = lm(value ~ 1 + mean - mean, data = .),
+     mod1 = lm(value ~ mean, data = .),
+     mod2 = lm(value ~ mean, data = .) # to have the same number of models (not sure if needed)
   )
 
 gradient %>% 
   ungroup() %>% 
   group_by(variable) %>%
-  do(AIC(.$mod0[[1]], .$mod1[[1]], .$mod2[[1]], .$mod3[[1]]))
+  do(AIC(.$mod0[[1]], .$mod1[[1]]))
 
 
 transplant <- responses %>%
@@ -178,8 +125,7 @@ transplant <- responses %>%
   filter(!is.na(value)) %>% 
   group_by(variable) %>% 
   #  mutate(value = scale(value)) %>% 
-  do(mod3 = lm(value ~ 1 + originSiteID - originSiteID, data = .),
-     mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
+  do(mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
      mod1 = lm(value ~ contrast + originSiteID, data = .),
      mod2 = lm(value ~ contrast * originSiteID, data = .)
   )
@@ -197,8 +143,7 @@ otc <- responses %>%
   filter(!is.na(value)) %>% 
   group_by(variable) %>% 
   #mutate(value = scale(value)) %>% 
-  do(mod3 = lm(value ~ 1 + originSiteID - originSiteID, data = .),
-     mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
+  do(mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
      mod1 = lm(value ~ contrast + originSiteID, data = .),
      mod2 = lm(value ~ contrast * originSiteID, data = .)
   )
@@ -209,20 +154,19 @@ otc %>%
   do(AIC(.$mod0[[1]], .$mod1[[1]], .$mod2[[1]])) %>% 
   spread(key = df, value = AIC)
 
+# bind model outputs and filter for relevant models, tidy up
 augmented_reg <- 
   bind_rows(Transplant = transplant, OTC = otc, Gradient = gradient, .id = "experiment") %>% 
   ungroup() %>% 
   group_by(variable, experiment) %>%
   do(
     bind_rows(
-      null.gradient = augment_aic(.$mod3[[1]]),
       null = augment_aic(.$mod0[[1]]), 
       effect = augment_aic(.$mod1[[1]]), 
       interaction = augment_aic(.$mod2[[1]]), 
       .id = "model")
   ) %>% 
-  filter(!(experiment == "Gradient" & model %in% c("effect", "interaction"))) %>% 
-  filter(!(experiment %in% c("OTC", "Transplant") & model %in% c("null.gradient"))) %>% 
+  filter(!(experiment == "Gradient" & model == "interaction")) %>% 
   filter(aic == min(aic)) %>% 
   ungroup() %>% 
   rename(response = "variable") %>% 
@@ -231,81 +175,12 @@ augmented_reg <-
     originSiteID = plyr::mapvalues(originSiteID, c("H", "A", "M", "L"), c("High alpine", "Alpine", "Middle", "Lowland")),
     response = plyr::mapvalues(response, c("richness", "evenness", "propGraminoid"), c("Richness", "Evenness", "Proportion Graminoid")),
     response = factor(response, levels = c("Richness", "Evenness", "Proportion Graminoid")),
-    contrast = if_else(experiment == "Gradient", mean, contrast)) %>% 
-  mutate(
-    model = ifelse(experiment == "Gradient" & model == "null", "effect", model),
-    model = factor(model, levels = c("null.gradient", "null", "effect", "interaction"), labels = c("No effect", "No effect", "Effect", "Interaction")))
+    contrast = if_else(experiment == "Gradient", mean, contrast),
+    model = factor(model, levels = c("null", "effect", "interaction"), labels = c("No effect", "Effect", "Interaction")))
 
 
-# Regression output
-tidy_model <- function(x){
-  tidy(x)
-}
 
-tidy_reg <- 
-  bind_rows(Transplant = transplant, OTC = otc, Gradient = gradient, .id = "experiment") %>% 
-  ungroup() %>% 
-  group_by(variable, experiment) %>%
-  do(
-    bind_rows(
-      null.gradient = tidy_model(.$mod3[[1]]),
-      null = tidy_model(.$mod0[[1]]), 
-      effect = tidy_model(.$mod1[[1]]), 
-      interaction = tidy_model(.$mod2[[1]]), 
-      .id = "model")
-  ) %>% 
-  #filter(!(experiment == "Gradient" & model != "interaction")) %>% 
-  filter(!(experiment == "Gradient" & model %in% c("effect", "interaction"))) %>% 
-  filter(!(experiment %in% c("OTC", "Transplant") & model %in% c("null.gradient"))) %>% 
-  ungroup() %>% 
-  rename(response = "variable") %>% 
-  filter(response %in% c("richness", "evenness", "propGraminoid")) %>% 
-  select(-p.value) %>% 
-  mutate(estimate = round(estimate, 2), std.error = round(std.error, 2), statistic = round(statistic, 2)) %>% 
-  mutate(
-    term = plyr::mapvalues(term, c("(Intercept)", "originSiteIDA", "originSiteIDM", "originSiteIDL", "contrast", "contrast:originSiteIDA", "contrast:originSiteIDM", "contrast:originSiteIDL"), c("Intercept", "Alpine", "Middle", "Lowland", "Contrast", "Contrast:Alpine", "Contrast:Middle", "Contrast:Lowland")),
-    response = factor(response, levels = c("richness", "evenness", "propGraminoid"), labels = c("Richness", "Evenness", "Proportion Graminoid"))) %>% 
-    mutate(
-           model = ifelse(experiment == "Gradient" & model == "null", "effect", model),
-           model = factor(model, levels = c("null.gradient", "null", "effect", "interaction"), labels = c("No effect", "No effect", "Effect", "Interaction")))
-  
-#write_csv(tidy_reg, path = "community/FinalFigures/RegressionOutput.csv", col_names = TRUE)
-
-
-# AIC output
-aic_model_ouput <- function(x){
-  glance(x)
-}
-
-aic_reg_output <- 
-  bind_rows(Transplant = transplant, OTC = otc, Gradient = gradient, .id = "experiment") %>% 
-  ungroup() %>% 
-  group_by(variable, experiment) %>%
-  do(
-    bind_rows(
-      null.gradient = aic_model_ouput(.$mod3[[1]]),
-      null = aic_model_ouput(.$mod0[[1]]), 
-      effect = aic_model_ouput(.$mod1[[1]]), 
-      interaction = aic_model_ouput(.$mod2[[1]]), 
-      .id = "model")
-  ) %>% 
-  select(-adj.r.squared, -sigma, -statistic, -p.value, -logLik, -BIC, -deviance) %>%
-  filter(!(experiment == "Gradient" & model %in% c("effect", "interaction"))) %>% 
-  filter(!(experiment %in% c("OTC", "Transplant") & model %in% c("null.gradient"))) %>% 
-  mutate(r.squared = round(r.squared, 3), AIC = round(AIC, 1)) %>% 
-  ungroup() %>% 
-  rename(response = "variable") %>% 
-  filter(response %in% c("richness", "evenness", "propGraminoid")) %>% 
-  mutate(
-    response = plyr::mapvalues(response, c("richness", "evenness", "propGraminoid"), c("Richness", "Evenness", "Proportion Graminoid")),
-    response = factor(response, levels = c("Richness", "Evenness", "Proportion Graminoid"))) %>% 
-  mutate(
-    model = ifelse(experiment == "Gradient" & model == "null", "effect", model),
-    model = factor(model, levels = c("null.gradient", "null", "effect", "interaction"), labels = c("No effect", "No effect", "Effect", "Interaction")))
-#write_csv(aic_reg_output, path = "community/FinalFigures/AICOutput.csv", col_names = TRUE)
-
-
-#preprocess points for plot
+# preprocess points for plot
 dd <- Transplant %>% 
   select(-diversity, -N1, -total_vascular) %>% 
   mutate(xvalue = ifelse(experiment == "Gradient", mean, contrast)) %>% 
@@ -319,6 +194,7 @@ dd <- Transplant %>%
   mutate(TTtreat = factor(TTtreat, levels = c("Control", "Local transplant", "OTC", "Transplant"))) %>% 
   filter(!(experiment == "Transplant" & originSiteID == "Lowland"))
 
+# plot Figure 1
 p <- ggplot(dd, aes(x = xvalue, y = value, colour = originSiteID, shape = TTtreat)) + 
   geom_jitter(height = 0, width = 0.1, size = 1.8) +
   geom_line(data = filter(augmented_reg, experiment != "Gradient"), aes(y = .fitted, x = contrast, colour = originSiteID, linetype = model), inherit.aes = FALSE) +
@@ -330,9 +206,6 @@ p <- ggplot(dd, aes(x = xvalue, y = value, colour = originSiteID, shape = TTtrea
   scale_linetype_manual(values = c("dotted", "dashed", "solid")) + 
   labs(x = "", y = "", colour = "Site", shape = "Treatment", linetype = "Model")
 
-#geom_smooth(data = filter(dd, experiment != "Gradient"), method = "lm", se = FALSE, aes(x = xvalue, y = value, colour = dummycolor), inherit.aes = FALSE, size = 0.6) +
-#  geom_smooth(data = filter(dd, experiment == "Gradient"), method = "lm", se = FALSE, aes(x = xvalue, y = value), inherit.aes = FALSE, size = 0.6, colour = "grey40") +
-
 CommunityPlot <- ggdraw(p) + 
   draw_label("Number of species", x = 0.015, y = 0.925, angle = 90,
            vjust = 1, hjust = 1, size = 14) +
@@ -340,9 +213,9 @@ CommunityPlot <- ggdraw(p) +
              vjust = 1, hjust = 1, size = 14) +
   draw_label("Proportion", x = 0.015, y = 0.3, angle = 90,
              vjust = 1, hjust = 1, size = 14) +
-  draw_label("Temperature °K", x = 0.36, y = 0.03,
+  draw_label("Temperature °C", x = 0.36, y = 0.03,
              vjust = 1, hjust = 1, size = 14) +
-  draw_label("Contrasts °K", x = 0.68, y = 0.03,
+  draw_label("Contrasts °C", x = 0.68, y = 0.03,
              vjust = 1, hjust = 1, size = 14)
 CommunityPlot
 ggsave(CommunityPlot, filename = "community/FinalFigures/CommunityPlot.jpg", height = 8, width = 10, dpi = 300)
