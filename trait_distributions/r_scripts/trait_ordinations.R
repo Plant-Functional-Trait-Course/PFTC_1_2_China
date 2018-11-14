8c61271a02045309283c7b841ee0b805dd639484
richardjtelford <richard.telford@bio.uib.no>
2018-11-14 01:03:47 +0100

    trait_distributions with drake

--- trait_distributions/r_scripts/trait_ordinations.R
+++ trait_distributions/r_scripts/trait_ordinations.R
@@ -1,79 +1,48 @@
+library("drake")
 library("tidyverse")
 library("vegan")
 library("ggvegan")
 
-cwm <- readRDS("trait_distributions/data/China_pftc_cwm.rds") %>% 
-  as_tibble() %>% 
-  mutate(site = factor(site, levels = c("H", "A", "M", "L"))) %>% 
-  rename(TTtreat = treatment) %>% 
-  mutate(TTtreat = factor(TTtreat, levels = c("C", "O", 1:4, "OTC"), labels = c("Control", "Local", "Warm1", "Cool1", "Warm3", "Cool3", "OTC")))
+source("trait_distributions/r_scripts/trait_ordinations_fun.R")
 
-#histograms
-cwm %>% 
-  filter(TTtreat %in% c("Control", "Local")) %>% 
-  ggplot(aes(x = mean, fill = site)) +
-  geom_histogram() +
-  facet_wrap(~ trait, scales = "free_x")
-
-cwm %>% 
-  filter(TTtreat %in% c("OTC")) %>% 
-  ggplot(aes(x = mean, fill = site)) +
-  geom_histogram() +
-  facet_wrap(~ trait, scales = "free_x")
+setting_plan <- drake_plan(
+  treat_colours = c("black", "grey50", "pink", "lightblue", "red", "green", "orange"),
+  strings_in_dots = "literals"
+  )
 
 
-cwm %>% 
-  filter(TTtreat %in% c("Warm1")) %>% 
-  ggplot(aes(x = mean, fill = site)) +
-  geom_histogram() +
-  facet_wrap(~ trait, scales = "free_x")
-
-cwm %>% 
-  filter(TTtreat %in% c("Cool1")) %>% 
-  ggplot(aes(x = mean, fill = site)) +
-  geom_histogram() +
-  facet_wrap(~ trait, scales = "free_x")
-
-### ordinations
-cwm_fat <- cwm %>% spread(key = trait, value = mean) 
-
-## control plots
-controls <- cwm_fat %>% filter(TTtreat %in% c("Control", "Local"))
-
-controls_pca <- rda(controls %>% select(-(turf:year)), scale = TRUE)
-
-control_sites <- bind_cols(
-  controls %>% select(turf:year), 
-  fortify(controls_pca, display = "sites"))
-
-control_traits <- fortify(controls_pca, display = "species")
-
-treat_colours <- c("black", "grey50", "pink", "lightgreen", "red", "green", "orange")
+cwm_datasets_plan <- drake_plan(
+  all_taxa = read_cwm("trait_distributions/data/China_pftc_cwm.rds"),
+  no_graminoids = read_cwm("trait_distributions/data/China_pftc_cwm_nongraminoids.rds"), 
+  strings_in_dots = "literals"
+)
 
 
-ggplot(control_sites, aes(x = PC1, y = PC2, shape = TTtreat, colour = site, group = turf)) +  
-  geom_point(aes(size = ifelse(year == min(year), "First", "Other"))) +
-  geom_path() +
-  #arrows
-  geom_segment(data = control_traits, 
-               aes(x = 0, y = 0, xend = PC1, yend = PC2), 
-               arrow = arrow(length = unit(0.2, "cm")), 
-               colour = "black",
-               inherit.aes = FALSE) +
-  geom_text(data = control_traits, 
-            aes(x = PC1 * 1.1,y = PC2 * 1.1, label = Label), 
-            size = 3,
-            inherit.aes = FALSE) +
-  coord_equal() +
-  scale_size_discrete(range = c(1, 2.5), limits = c("Other", "First"), breaks = c("First", "Other")) +
-  
-  scale_colour_brewer(palette = "RdBu", direction = -1) +
-  scale_shape_manual(values = c(16, 21)) +
-  scale_x_continuous(expand = c(.15, 0)) +
-  labs(x = "PC 1", y = "PC 2", shape = "Treatment", colour = "Site", size = "Year", title = "Controls")
+methods_plan <- drake_plan(
+  AH = twoSites(dataset__, low = "A", high = "H", treat_colours = treat_colours),
+  MA = twoSites(dataset__, low = "M", high = "A", treat_colours = treat_colours),
+  LM = twoSites(dataset__, low = "L", high = "M", treat_colours = treat_colours),
+  LH = twoSites(dataset__, low = "L", high = "H", treat_colours = treat_colours), 
+  strings_in_dots = "literals"
+)
 
-ggsave(filename = "cwm_trait_control_pca.png", width = 6, height = 5)
+cwm_analyses <- evaluate_plan(
+  methods_plan, 
+  wildcard = "dataset__",
+  values = cwm_datasets_plan$target
+)
 
+cwm_report <- drake_plan(
+  report = rmarkdown::render(input = knitr_in("trait_distributions/Rmd/trait_ordinations.Rmd")), 
+  strings_in_dots = "literals"
+)
 
+my_plan <- bind_rows(cwm_report, cwm_datasets_plan, cwm_analyses, setting_plan)
 
+conf <- drake_config(my_plan)
+make(my_plan)
+
+vis_drake_graph(conf, targets_only = TRUE)
+
+system("evince trait_distributions/Rmd/trait_ordinations.pdf", wait = FALSE)
 
