0ae25bae01c963ab42171f2d85d8cc2aa71bbf7f
richardjtelford <richard.telford@bio.uib.no>
2018-11-14 14:22:24 +0100

    moving string_in_dots argument to drake setup

--- trait_distributions/r_scripts/trait_ordinations.R
+++ trait_distributions/r_scripts/trait_ordinations.R
@@ -3,18 +3,18 @@ library("tidyverse")
 library("vegan")
 library("ggvegan")
 
+pkgconfig::set_config("drake::strings_in_dots" = "literals")
+
 source("trait_distributions/r_scripts/trait_ordinations_fun.R")
 
 setting_plan <- drake_plan(
-  treat_colours = c("black", "grey50", "pink", "lightblue", "red", "green", "orange"),
-  strings_in_dots = "literals"
+  treat_colours = c("black", "grey50", "pink", "lightblue", "red", "green", "orange")
   )
 
 
 cwm_datasets_plan <- drake_plan(
   all_taxa = read_cwm("trait_distributions/data/China_pftc_cwm.rds"),
-  no_graminoids = read_cwm("trait_distributions/data/China_pftc_cwm_nongraminoids.rds"), 
-  strings_in_dots = "literals"
+  no_graminoids = read_cwm("trait_distributions/data/China_pftc_cwm_nongraminoids.rds")
 )
 
 
@@ -22,8 +22,7 @@ methods_plan <- drake_plan(
   AH = twoSites(dataset__, low = "A", high = "H", treat_colours = treat_colours),
   MA = twoSites(dataset__, low = "M", high = "A", treat_colours = treat_colours),
   LM = twoSites(dataset__, low = "L", high = "M", treat_colours = treat_colours),
-  LH = twoSites(dataset__, low = "L", high = "H", treat_colours = treat_colours), 
-  strings_in_dots = "literals"
+  LH = twoSites(dataset__, low = "L", high = "H", treat_colours = treat_colours)
 )
 
 cwm_analyses <- evaluate_plan(
@@ -33,8 +32,7 @@ cwm_analyses <- evaluate_plan(
 )
 
 cwm_report <- drake_plan(
-  report = rmarkdown::render(input = knitr_in("trait_distributions/Rmd/trait_ordinations.Rmd")), 
-  strings_in_dots = "literals"
+  report = rmarkdown::render(input = knitr_in("trait_distributions/Rmd/trait_ordinations.Rmd"))
 )
 
 my_plan <- bind_rows(cwm_report, cwm_datasets_plan, cwm_analyses, setting_plan)
