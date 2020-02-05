689b2f0b4b98207386a38a23a242b316c70b35ea
audhalbritter <audhalbritter.com>
2020-02-05 15:49:44 +0100

    download data from osf fix figure

--- climate/R/TomstLoggers.R
+++ climate/R/TomstLoggers.R
@@ -2,15 +2,18 @@
 ### READ IN TOMST LOGGERS ###
 ###########################
 
+
 #load libraries
 library("tidyverse")
 library("readr")
 library("readxl")
 library("lubridate")
+#devtools::install_github("Between-the-Fjords/dataDownloader")
+library("dataDownloader")
 
 
 ### Read in files
-files <- dir(path = "climate/Tomst_2020/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
+files <- dir(path = "climate/data_raw/Tomst_2020/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
 
 # Function to read in data
 temp <- map_df(set_names(files), function(file) {
@@ -30,22 +33,43 @@ temp <- map_df(set_names(files), function(file) {
 
 temp <- temp %>% 
   pivot_longer(cols = c(SoilTemperature, GroundTemperature, AirTemperature), names_to = "Variable", values_to = "Temperature") %>% 
-  mutate(Month = month(DateTime),
-         Treatment = recode(Treatment, "C" = "Control", "W" = "OTC"))
+  mutate(Treatment = recode(Treatment, "C" = "Control", "W" = "OTC"),
+         YearMonth = ymd(paste(year(DateTime), month(DateTime), "15"))) 
   
-ggplot(temp, aes(x = DateTime, y = Temperature, colour = Treatment)) +
-  geom_line() +
-  scale_color_manual(values = c("grey50", "red")) +
-  facet_grid(Variable ~ Treatment) +
-  theme_bw()
 
-temp %>% 
-  group_by(Month, Variable, Treatment) %>% 
+## ----TomstOTCPlot
+#### Load clean data
+# Download OSF
+#Download files from OSF
+# get_file(node = "f3knq",
+#          file = "China_2019_Climate_TomtsLogger.csv",
+#          path = "climate/data_cleaned",
+#          remote_path = "Climate")
+
+# Read in data
+temp <- read_csv(file = "climate/data_cleaned/China_2019_Climate_TomstLogger.csv")
+
+
+# Plotting  
+# ggplot(temp, aes(x = DateTime, y = Temperature, colour = Treatment)) +
+#   geom_line() +
+#   scale_color_manual(values = c("grey50", "red")) +
+#   facet_grid(Variable ~ Treatment) +
+#   theme_bw()
+
+
+TomstOTC <- temp %>% 
+  group_by(YearMonth, Variable, Treatment) %>% 
   summarise(MeanTemperature = mean(Temperature), SETemperature = sd(Temperature)/sqrt(n())) %>% 
-  ggplot(aes(x = Month, y = MeanTemperature, ymin = MeanTemperature - SETemperature, ymax = MeanTemperature + SETemperature, colour = Treatment)) +
+  ggplot(aes(x = YearMonth, y = MeanTemperature, ymin = MeanTemperature - SETemperature, ymax = MeanTemperature + SETemperature, colour = Treatment)) +
+  #scale_x_yearmon(n = 2) +
+  scale_x_date(date_breaks="1 month", date_labels="%Y %m") +
   geom_line() +
   geom_point() +
-  geom_errorbar(width = 0.3) +
-  scale_color_manual(values = c("grey50", "red")) +
+  geom_errorbar(width = 0.1) +
+  labs(x = "", y = "Mean temperautre in °C") +
+  scale_color_manual(values = c("grey50", "orange")) +
   facet_wrap(~ Variable) +
   theme_bw()
+TomstOTC
+#ggsave(TomstOTC, filename = "TomstOTC.jpg", height = 6, width = 10, dpi = 300)
