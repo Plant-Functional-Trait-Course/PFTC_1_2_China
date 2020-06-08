09d3c86cc781c2116c86d66f20940483a506fc4d
audhalbritter <me@audhalbritter.com>
2020-06-08 15:34:37 +0200

    delete all the community corrections, fixed in database import code

--- traits/R/Taxonomy_harmonizer.R
+++ traits/R/Taxonomy_harmonizer.R
@@ -4,8 +4,6 @@
 # speciesName = old names in community data
 # new_taxon = new name used in both data sets
 
-taxon_harmonizer <- tibble(Taxon = c("Anaphalis flavescens", "Gentiana pseudoaquatica", "Halenia elliptica", "Juncus leucomelas", "Luzula multiflora", "Maianthemum henryi", "Poa polycolea", "Prunella vulgaris", "Salix souliei", "Carex nibigella") ,
-                           
-                           speciesName = c("Anaphalis aureopunctata", "Gentiana aquatica var. pseudoaquatica", "Halenia corniculata", "Juncus leucanthus", "Luzula spp", "Maianthemum japonicum", "Poa spp", "Prunella hispida", "Salix brachista", "Carex nibigella"),
-                           
-                           new_taxon = c("Anaphalis flavescens", "Gentiana pseudoaquatica", "Halenia elliptica", "Juncus leucomelas", "Luzula spp", "Maianthemum henryi", "Poa polycolea", "Prunella vulgaris subsp. hispida", "Salix souliei", "Carex spectabilis"))
+taxon_harmonizer <- tibble(Taxon = c("Luzula multiflora") ,
+
+                           new_taxon = c("Luzula spp"))
